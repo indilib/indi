8dcb6048b3ee1893284eed8854e38d3ed1ead9d7
Gerry Rozema <gerryr@rozeware.com>
2010-12-08 17:14:06 +0000

    Adding the test client code

---
+++ 3rdparty/gerry/testclient/main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "testclient.h"
+
+int main()
+{
+    bool rc;
+    testclient *client;
+
+    client=new testclient();
+
+    client->setServer("localhost",7624);
+    rc=client->connectServer();
+    sleep(1);  //  liet it connect
+    if(rc) client->dotest();
+    printf("program exit\n");
+
+    return 0;
+}
