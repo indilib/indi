e14b88db2c061546ed0daabb17160af9d11d14cd
Jasem Mutlaq <mutlaqja@ikarustech.com>
2011-12-27 21:16:15 +0000

    cleanup of old structure

--- indi-gphoto/gphoto_readimage.h
+++
@@ -1,5 +0,0 @@
-#ifndef _GPHOTO_READIMAGE_H_
-#define _GPHOTO_READIMAGE_H_
-extern int read_dcraw(const char *filename, void **memptr, size_t *memsize);
-extern int read_jpeg(const char *filename, void **memptr, size_t *memsize);
-#endif
