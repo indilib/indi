6d3bf84205f2c990997d19cd925e95935a5f8fdb
Jasem Mutlaq <mutlaqja@ikarustech.com>
2012-09-17 19:34:48 +0000

    correct dir name

--- 3rdparty/indi-qhy/qhy5_driver.h
+++ 3rdparty/indi-qhy/qhy5_driver.h
@@ -1,20 +1,84 @@
 #ifndef _QHY5_DRIVER_H_
 #define _QHY5_DRIVER_H_
-struct _qhy5_driver;
-typedef struct _qhy5_driver qhy5_driver;
-enum {
-	QHY_NORTH = 0x20,
-	QHY_SOUTH = 0x40,
-	QHY_EAST  = 0x10,
-	QHY_WEST  = 0x80,
+
+#include <libindi/indiccd.h>
+#include <libindi/indiusbdevice.h>
+
+#define USHORT unsigned short int
+#define BYTE unsigned char
+
+#define IMAGE_CCD 0
+#define GUIDE_CCD 1
+
+enum
+{
+    QHY_NORTH = 0x20,
+    QHY_SOUTH = 0x40,
+    QHY_EAST  = 0x10,
+    QHY_WEST  = 0x80,
 };
 
-extern int qhy5_start_exposure(qhy5_driver *handle, unsigned int exposure);
-extern int qhy5_read_exposure(qhy5_driver *handle);
-extern void *qhy5_get_row(qhy5_driver *handle, int row);
-extern int qhy5_set_params(qhy5_driver *handle, int width, int height, int binw, int binh, int offw, int offh, int gain, int *pixw, int *pixh);
-extern qhy5_driver *qhy5_open();
-extern int qhy5_close(qhy5_driver *handle);
-extern int qhy5_query_capabilities(qhy5_driver *handle, int *width, int *height, int *binw, int *binh, int *gain);
-extern int qhy5_timed_move(qhy5_driver *qhy5, int direction, int duration_msec);
+
+/*struct _qhy5_driver {
+    usb_dev_handle *handle;
+
+    void *image;
+    size_t imagesize;
+};*/
+
+
+class QHY5Driver :  public INDI::USBDevice
+{
+    public:
+        QHY5Driver();
+        //  variables that we inherit from the indi ccd stuff
+        //  and we need a local copy for stand-alone use
+
+        int width;
+        int height;
+        int binw;
+        int binh;
+        int gain;
+        int offw;
+        int offh;
+        int bpp;
+
+        int impixw,impixh;
+
+        bool hasGuide;
+        bool hasST4;
+
+        void *imageBuffer;
+        int imageBufferSize;
+
+
+        bool Connect();
+        bool Disconnect();
+
+
+/*    int ResetCamera();
+ *    int ClearPixels(int flags,int cam);
+        int LatchPixels(int flags,int cam,int xoffset,int yoffset,int width,int height,int xbin,int ybin);
+        int ExposePixels(int flags,int cam,int xoffset,int yoffset,int width,int height,int xbin,int ybin, int ms);
+        int ReadPixels(char *,int);
+
+        int pulseGuide();
+
+        virtual int SetParams(int XRes,int YRes,int CamBits,float pixwidth,float pixheight);
+        virtual int SetGuideParams(int XRes,int YRes,int CamBits,float pixwidth,float pixheight);
+        virtual int SetInterlaced(bool);*/
+
+        int SetParams(int in_width, int in_height, int in_binw, int in_binh, int in_offw, int in_offh, int in_gain, int *pixw, int *pixh);
+        int GetDefaultParam(int *width, int *height, int *binw, int *binh, int *gain);
+
+        void *GetRow(int row);
+        int ReadExposure();
+        int StartExposure(unsigned int exposure);
+
+        int Pulse(int direction, int duration_msec);
+        int ResetCamera();
+
+
+};
+
 #endif
