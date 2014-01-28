7d74c10757281206c6a6e83ff0db0da7ba1564cf
Peter Polakovic <peter.polakovic@cloudmakers.eu>
2014-01-28 08:58:54 +0000

    compiler warnings fixed

--- 3rdparty/indi-sx/sxccdtest.cpp
+++ 3rdparty/indi-sx/sxccdtest.cpp
@@ -118,9 +118,9 @@ int main() {
     i = sxReadPixels(handle, pixels, 10*10);
     cout << "sxReadPixels() -> " << i << endl << endl;
 
-    for (int i=0; i<10; i++) {
-      for (int j=0; j<10; j++)
-        cout << pixels[i*10+j] << " ";
+    for (int x=0; x<10; x++) {
+      for (int y=0; y<10; y++)
+        cout << pixels[x*10+y] << " ";
       cout << endl;
     }
     cout << endl;
@@ -141,9 +141,9 @@ int main() {
       i = sxReadPixels(handle, pixels, 10*10);
       cout << "sxReadPixels() -> " << i << endl << endl;
 
-      for (int i=0; i<10; i++) {
-        for (int j=0; j<10; j++)
-          cout << pixels[i*10+j] << " ";
+      for (int x=0; x<10; x++) {
+        for (int y=0; y<10; y++)
+          cout << pixels[x*10+y] << " ";
         cout << endl;
       }
       cout << endl;
