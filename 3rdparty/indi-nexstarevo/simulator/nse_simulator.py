be88d38a53ca4add87e230829a8f65479f58b326
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2016-06-16 20:53:59 +0200

    NSE: Refactor simulator. First version good enough for the Celestron app to connect.

--- 3rdparty/indi-nexstarevo/simulator/nse_simulator.py
+++ 3rdparty/indi-nexstarevo/simulator/nse_simulator.py
@@ -1,15 +1,20 @@
-#!/bin/env python
+#!/bin/env python3
 
 import asyncio
 import signal
 import socket
+import sys
 from socket import SOL_SOCKET, SO_BROADCAST, SO_REUSEADDR
- 
+from nse_telescope import NexStarScope
+
+telescope = NexStarScope()
+
 async def broadcast(sport=2000, dport=55555, host='255.255.255.255', seconds_to_sleep=5):
     broadcast.keep_running=True
     sck = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sck.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     sck.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+    # Fake msg. The app does not care for the payload
     msg = 110*b'X'
     sck.bind(('',sport))
     while broadcast.keep_running :
@@ -17,93 +22,138 @@ async def broadcast(sport=2000, dport=55555, host='255.255.255.255', seconds_to_
         print('Send {0} bytes, sleeping for: {1} seconds'.format(bn, seconds_to_sleep,))
         await asyncio.sleep(seconds_to_sleep)
     print('App connected - stopping broadcast')
- 
 
-def print_command(cmd):
-    return 'Command: %02x->%02x [%02x] len:%d: data:%r' % (cmd[1], cmd[2], cmd[3], cmd[0], cmd[4:-1])
-
-def decode_command(cmd):
-    return (cmd[3], cmd[1], cmd[2], cmd[0], cmd[4:-1], cmd[-1])
-
-def split_cmds(data):
-    return [cmd for cmd in data.split(b';') if len(cmd)]
-
-def make_checksum(data):
-    return ((~sum([c for c in bytes(data)]) + 1) ) & 0xFF
-
-ACK_CMDS=[0x02,0x04,0x06,0x24,]
-
-def ack_cmd(cmd):
-    c,f,t,l,d,cs=decode_command(cmd)
-    rsp=b''
-    if c in ACK_CMDS :
-        rsp=b';\x03'+bytes((t,f,c))
-        rsp+=bytes((make_checksum(rsp[1:]),))
-    return rsp
+async def timer(seconds_to_sleep=1):
+    timer.keep_running = True
+    while timer.keep_running :
+        await asyncio.sleep(seconds_to_sleep)
+        telescope.tick(seconds_to_sleep)
 
 async def handle_port2000(reader, writer):
-    data = await reader.read(100)
-    message = data.decode()
-    addr = writer.get_extra_info('peername')
-    print("-> Scope received %r from %r" % (message, addr))
-    if message == '$$$' :
-        broadcast.keep_running=False
-        print('App from {0} connected. Terminating broadcast and starting main loop.'.format(addr))
-        data = b'CMD\r\n'
-        print("<- Scope sending: %r" % data )
-        writer.write(data)
-        await writer.drain()
-
-    initial = True
-    while initial :
-        data = await reader.read(256)
-        message = data.decode().strip()
-        addr = writer.get_extra_info('peername')
-        print("-> Scope received %r from %r" % (message, addr))
-        print("<- Scope sending: %r" % message)
-        writer.write(data)
-        if message == 'exit' :
-            initial = False
-            data = b'\r\nEXIT\r\n'
-            print("<- Server sending: %r" % data )
-            writer.write(data)                    
-        else :
-            data = b'\r\nAOK\r\n<2.40-CEL> '
-            print("<- Server sending: %r" % data )
-            writer.write(data)        
-        await writer.drain()
-
-    print('Initial negotiations finished starting scope mode')
+    '''
+    This function handles initial communication with the WiFly module and
+    delegates the real job of simulating the scope to the NexStarScope class.
+    It also handles all the dirty details of actual communication.
+    '''
+    
+    # The WiFly module is initially in the transparent mode and just passes
+    # the data to the serial connection. Unless the '$$$' sequence is detected.
+    # Then it switches to the command mode until the exit command is issued.
+    # The $$$ should be guarded by the 1s silence.
+    transparent=True
+    retry = 5
+    # Endless comm loop.
     while True :
-        try :
-            data = await reader.read(256)
-            addr = writer.get_extra_info('peername')
-            if len(data)>1 and data[0]==0x3b :
-                for cmd in split_cmds(data):
-                    print("-> Scope received %s from %r." % (print_command(cmd), addr))
-                    c,f,t,l,d,s=decode_command(cmd)
-                    #print(c,f,t,l,d,s)
-                    if c == 0xfe :
-                        rpl = bytes.fromhex('3b032010fecf3b071020fe070b13ecba')
-                    elif c == 0x05 :
-                        rpl = bytes.fromhex('3b03201005c83b0510200516852b')
-                    else :
-                        rpl = b';' + cmd + ack_cmd(cmd)
-                    for c in split_cmds(rpl):
-                        print("<- Scope sending:", print_command(c))
-                    writer.write(rpl)
-                await writer.drain()
+        data = await reader.read(1024)
+        if not data :
+            if retry:
+                await asyncio.sleep(1)
+                retry -= 1
+                continue
             else :
-                if len(data):
-                    print("-> Scope received %r from %r. IGNORE" % (data, addr))
-                else :
-                    print("-- Terminating connection")
-                    writer.close()
-                    return
-        except :
-            print("EXception -- Terminating connection")
-            writer.close()
-            raise
+                writer.close()
+                timer.keep_running = False
+                broadcast.keep_running = False
+                return
+        retry = 5
+        addr = writer.get_extra_info('peername')
+        #print("-> Scope received %r from %r." % (data, addr))
+        resp = b''
+        if transparent :
+            if data[:3]==b'$$$' :
+                # Enter command mode
+                transparent = False
+                broadcast.keep_running=False
+                print('App from {0} connected. Terminating broadcast and starting main loop.'.format(addr))
+                resp = b'CMD\r\n'
+            else :
+                # pass it on to the scope for handling
+                resp = telescope.handle_msg(data)
+        else :
+            # We are in command mode detect exit and get out.
+            # Otherwise just echo what we got and ack.
+            try :
+                message = data.decode('ascii').strip()
+            except UnicodeError :
+                # The data is invalid ascii - ignore it
+                pass
+            if message == 'exit' :
+                # get out of the command mode
+                transparent = True
+                resp = data + b'\r\nEXIT\r\n'
+            else :
+                resp = data + b'\r\nAOK\r\n<2.40-CEL> '
+        if resp :
+            #print("<- Server sending: %r" % resp )
+            writer.write(resp)
+            await writer.drain()
+        
+        
+        
+        
+        
+        
+#    # First part - the WiFly modules waits for 
+#        message = data.decode()
+#        print("-> Scope received %r from %r" % (message, addr))
+#        if message == '$$$' :
+#            broadcast.keep_running=False
+#            print('App from {0} connected. Terminating broadcast and starting main loop.'.format(addr))
+#            data = b'CMD\r\n'
+#            print("<- Scope sending: %r" % data )
+#            writer.write(data)
+#            await writer.drain()
+
+#    initial = True
+#    while initial :
+#        data = await reader.read(256)
+#        message = data.decode().strip()
+#        addr = writer.get_extra_info('peername')
+#        print("-> Scope received %r from %r" % (message, addr))
+#        print("<- Scope sending: %r" % message)
+#        writer.write(data)
+#        if message == 'exit' :
+#            initial = False
+#            data = b'\r\nEXIT\r\n'
+#            print("<- Server sending: %r" % data )
+#            writer.write(data)                    
+#        else :
+#            data = b'\r\nAOK\r\n<2.40-CEL> '
+#            print("<- Server sending: %r" % data )
+#            writer.write(data)        
+#        await writer.drain()
+
+#    print('Initial negotiations finished starting scope mode')
+#    while True :
+#        try :
+#            data = await reader.read(256)
+#            addr = writer.get_extra_info('peername')
+#            if len(data)>1 and data[0]==0x3b :
+#                for cmd in split_cmds(data):
+#                    print("-> Scope received %s from %r." % (print_command(cmd), addr))
+#                    c,f,t,l,d,s=decode_command(cmd)
+#                    #print(c,f,t,l,d,s)
+#                    if c == 0xfe :
+#                        rpl = bytes.fromhex('3b032010fecf3b071020fe070b13ecba')
+#                    elif c == 0x05 :
+#                        rpl = bytes.fromhex('3b03201005c83b0510200516852b')
+#                    else :
+#                        rpl = b';' + cmd + ack_cmd(cmd)
+#                    for c in split_cmds(rpl):
+#                        print("<- Scope sending:", print_command(c))
+#                    writer.write(rpl)
+#                await writer.drain()
+#            else :
+#                if len(data):
+#                    print("-> Scope received %r from %r. IGNORE" % (data, addr))
+#                else :
+#                    print("-- Terminating connection")
+#                    writer.close()
+#                    return
+#        except :
+#            print("EXception -- Terminating connection")
+#            writer.close()
+#            raise
 
 def signal_handler(signal, frame):  
     loop.stop()
@@ -114,5 +164,5 @@ signal.signal(signal.SIGINT, signal_handler)
 loop = asyncio.get_event_loop()
 scope = asyncio.start_server(handle_port2000, host='', port=2000)
 
-loop.run_until_complete(asyncio.wait([broadcast(), scope]))
+loop.run_until_complete(asyncio.wait([broadcast(), timer(), scope]))
 loop.close()
---
+++ 3rdparty/indi-nexstarevo/simulator/nse_telescope.py
@@ -0,0 +1,351 @@
+#!/bin/env python3
+
+import struct
+
+
+# ID tables
+targets={'ANY':0x00,
+         'MB' :0x01,
+         'HC' :0x04,
+         'UKN1':0x05,
+         'HC+':0x0d,
+         'AZM':0x10,
+         'ALT':0x11,
+         'APP':0x20,
+         'GPS':0xb0,
+         'TRG1': 0xb4,
+         'WiFi':0xb5,
+         'BAT':0xb6,
+         'CHG':0xb7,
+         'LIGHT':0xbf
+        }
+trg_names={value:key for key, value in targets.items()}
+        
+control={
+         'HC' :0x04,
+         'HC+':0x0d,
+         'APP':0x20,
+        }
+
+commands={
+          'MC_GET_POSITION':0x01,
+          'MC_GOTO_FAST':0x02,
+          'MC_SET_POSITION':0x04,
+          'MC_GET_???':0x05,
+          'MC_SET_POS_GUIDERATE':0x06,
+          'MC_SET_NEG_GUIDERATE':0x07,
+          'MC_LEVEL_START':0x0b,
+          'MC_SET_POS_BACKLASH':0x10,
+          'MC_SET_NEG_BACKLASH':0x11,
+          'MC_GOTO_SLOW':0x17,
+          'MC_AT_INDEX':0x18,
+          'MC_SEEK_INDEX':0x19,
+          'MC_CMD21':0x21,
+          'MC_CMD23':0x23,
+          'MC_MOVE_POS':0x24,
+          'MC_MOVE_NEG':0x25,
+          'MC_GET_POS_BACKLASH':0x40,
+          'MC_GET_NEG_BACKLASH':0x41,
+          'MC_GET_AUTOGUIDE_RATE':0x47,
+          'MC_GET_APPROACH':0xfc,
+          'MC_SET_APPROACH':0xfd,
+          'GET_VER':0xfe,
+         }
+cmd_names={value:key for key, value in commands.items()}
+
+ACK_CMDS=[0x02,0x04,0x06,0x24,]
+
+def print_command(cmd):
+    if cmd[2] in (0x10, 0x20):
+        try :
+            return 'Command: %s->%s [%s] len:%d: data:%r' % (
+                        trg_names[cmd[1]], 
+                        trg_names[cmd[2]], 
+                        cmd_names[cmd[3]], cmd[0], cmd[4:-1])
+        except KeyError :
+            pass
+    try :
+        return 'Command: %s->%s [%02x] len:%d: data:%r' % (
+                    trg_names[cmd[1]], 
+                    trg_names[cmd[2]], 
+                    cmd[3], cmd[0], cmd[4:-1])
+    except KeyError :
+        pass
+        
+    return 'Command: %02x->%02x [%02x] len:%d: data:%r' % (
+                cmd[1], cmd[2], cmd[3], cmd[0], cmd[4:-1])
+            
+
+def decode_command(cmd):
+    return (cmd[3], cmd[1], cmd[2], cmd[0], cmd[4:-1], cmd[-1])
+
+def split_cmds(data):
+    # split the data to commands
+    # the initial byte b'\03b' is removed from commands
+    return [cmd for cmd in data.split(b';') if len(cmd)]
+
+def make_checksum(data):
+    return ((~sum([c for c in bytes(data)]) + 1) ) & 0xFF
+
+
+def ack_cmd(cmd):
+    c,f,t,l,d,cs=decode_command(cmd)
+    rsp=b''
+    if c in ACK_CMDS :
+        rsp=b';\x03'+bytes((t,f,c))
+        rsp+=bytes((make_checksum(rsp[1:]),))
+    return rsp
+
+def f2dms(f):
+    '''
+    Convert fraction of the full rotation to DMS triple (degrees).
+    '''
+    d=360*f
+    dd=int(d)
+    mm=int((d-dd)*60)
+    ss=(d-dd-mm/60)*3600
+    return dd,mm,ss
+
+def parse_pos(d):
+    '''
+    Parse first three bytes into the DMS string
+    '''
+    if len(d)>=3 :
+        pos=struct.unpack('!i',b'\x00'+d[:3])[0]/2**24
+        return u'%03d°%02d\'%04.1f"' % f2dms(pos)
+    else :
+        return u''
+
+
+
+class NexStarScope:
+    
+    __mcfw_ver=(7,11,5100//256,5100%256)
+    __hcfw_ver=(5,28,5300//256,5300%256)
+    
+    trg=('MB', 'HC', 'UKN1', 'HC+', 'AZM', 'ALT', 'APP', 
+            'GPS', 'WiFi', 'BAT', 'CHG', 'LIGHT')
+    
+    def __init__(self, ALT=0.0, AZM=0.0):
+        self.__alt=ALT
+        self.__azm=AZM
+        self.alt_rate=0
+        self.azm_rate=0
+        self.alt_approach=0
+        self.azm_approach=0
+        self._other_handlers = {
+            0x10: NexStarScope.cmd_0x10,
+            0x18: NexStarScope.cmd_0x18,
+            0xfe: NexStarScope.send_ack,
+        }
+        self._mc_handlers = {
+          0x01 : NexStarScope.get_position,
+          0x02 : NexStarScope.goto_fast,
+          0x04 : NexStarScope.set_position,
+          0x05 : NexStarScope.cmd_0x05,
+          0x06 : NexStarScope.set_pos_guiderate,
+          0x07 : NexStarScope.set_neg_guiderate,
+          0x0b : NexStarScope.level_start,
+          0x10 : NexStarScope.set_pos_backlash,
+          0x11 : NexStarScope.set_neg_backlash,
+          0x17 : NexStarScope.goto_slow,
+          0x18 : NexStarScope.at_index,
+          0x19 : NexStarScope.seek_index,
+          0x21 : NexStarScope.cmd_0x21,
+          0x23 : NexStarScope.cmd_0x23,
+          0x24 : NexStarScope.move_pos,
+          0x25 : NexStarScope.move_neg,
+          0x39 : NexStarScope.send_ack,
+          0x3a : NexStarScope.set_cordwrap_pos,
+          0x40 : NexStarScope.get_pos_backlash,
+          0x41 : NexStarScope.get_neg_backlash,
+          0x47 : NexStarScope.get_autoguide_rate,
+          0xfc : NexStarScope.get_approach,
+          0xfd : NexStarScope.set_approach,
+          0xfe : NexStarScope.fw_version,
+        }
+
+    def send_ack(self, data, snd, rcv):
+        return b''
+
+    def cmd_0x21(self, data, snd, rcv):
+        return bytes.fromhex('0fa01194')
+
+    def cmd_0x23(self, data, snd, rcv):
+        return b'\x00'
+
+    def cmd_0x10(self, data, snd, rcv):
+        if rcv == 0xbf : # LIGHT
+            return b'\xff'
+        elif rcv == 0xb7 : # CHG
+            return b'\x00'
+        elif rcv == 0xb6 : # BAT
+            return bytes.fromhex('0102009edfe5') # ????!!!!
+        else :
+            return b''
+
+    def cmd_0x18(self, data, snd, rcv):
+        if rcv == 0xb6 : # BAT
+            return b'\x07\xd0' # 2000mA ?
+
+
+    def get_position(self, data, snd, rcv):
+        try :
+            trg = trg_names[rcv]
+        except KeyError :
+            trg = '???'
+        if trg == 'ALT':
+            return struct.pack('!i',int(self.alt))[1:]
+        else :
+            return struct.pack('!i',int(self.azm))[1:]
+
+    def goto_fast(self, data, snd, rcv):
+        return b''
+
+    def set_position(self,data, snd, rcv):
+        return b''
+
+    def cmd_0x05(self, data, snd, rcv):
+        return bytes.fromhex('1685')
+
+    def set_pos_guiderate(self, data, snd, rcv):
+        return b''
+
+    def set_neg_guiderate(self, data, snd, rcv):
+        return b''
+
+    def level_start(self, data, snd, rcv):
+        return b''
+
+    def set_pos_backlash(self, data, snd, rcv):
+        return b''
+
+    def set_neg_backlash(self, data, snd, rcv):
+        return b''
+
+    def goto_slow(self, data, snd, rcv):
+        return b''
+
+    def at_index(self, data, snd, rcv):
+        return b'\x00'
+
+    def seek_index(self, data, snd, rcv):
+        return b''
+
+    def move_pos(self, data, snd, rcv):
+        r=int(data[0])
+        if trg_names[rcv] == 'ALT':
+            self.alt_rate = r
+        else :
+            self.azm_rate = r
+        return b''
+
+    def move_neg(self, data, snd, rcv):
+        r=int(data[0])
+        if trg_names[rcv] == 'ALT':
+            self.alt_rate = -r
+        else :
+            self.azm_rate = -r
+        return b''
+
+    def set_cordwrap_pos(self, data, snd, rcv):
+        self.cordwrap_pos=struct.unpack('!i',b'\x00'+data[:3])[0]
+        return b''
+
+    def get_pos_backlash(self, data, snd, rcv):
+        return b'\x00'
+
+    def get_neg_backlash(self, data, snd, rcv):
+        return b'\x00'
+
+    def get_autoguide_rate(self, data, snd, rcv):
+        return b'\xf0'
+
+    def get_approach(self, data, snd, rcv):
+        try :
+            trg = trg_names[rcv]
+        except KeyError :
+            trg = '???'
+        if trg == 'ALT':
+            return bytes((self.alt_approach,))
+        else :
+            return bytes((self.azm_approach,))
+            
+    def set_approach(self, data, snd, rcv):
+        try :
+            trg = trg_names[rcv]
+        except KeyError :
+            trg = '???'
+        if trg == 'ALT':
+            self.alt_approach=data[0]
+        else :
+            self.azm_approach=data[0]
+        return b''
+
+    def fw_version(self, data, snd, rcv):
+        try :
+            trg = trg_names[rcv]
+        except KeyError :
+            trg = '???'
+        if trg in ('ALT','AZM'):
+            return bytes(NexStarScope.__mcfw_ver)
+        elif trg in ('HC', 'HC+'):
+            return bytes(NexStarScope.__hcfw_ver)
+        else :
+            return b''
+
+    def tick(self, interval):
+        self.alt += self.alt_rate
+        self.azm += self.azm_rate
+        print('Scope at: ALT: %d  AZM: %d' % (self.alt, self.azm))
+
+    @property
+    def alt(self):
+        return self.__alt
+        
+    @alt.setter
+    def alt(self,ALT):
+        self.__alt=ALT
+        
+        
+    @property
+    def azm(self):
+        return self.__azm
+    
+    @azm.setter
+    def azm(self,AZM):
+        self.__azm=AZM
+
+    def handle_cmd(self, cmd):
+        print("-> Scope received %s." % (print_command(cmd)))
+        c,f,t,l,d,s=decode_command(cmd)
+        resp=b''
+        if make_checksum(cmd[:-1]) != s :
+            print("Wrong checksum. Ignoring.")
+        else :
+            resp = b';' + cmd
+            if t in (0x10, 0x11):
+                handlers=self._mc_handlers
+            else :
+                handlers=self._other_handlers
+                
+            if c in handlers :
+                r = handlers[c](self,d,f,t)
+                r = bytes((len(r)+3,t,f,c)) + r
+                resp += b';' + r + bytes((make_checksum(r),))
+                print('Response: %r' % resp)
+            else :
+                print('Scope got unknown command %02x' % c)
+                
+        return resp
+
+    def handle_msg(self, msg):
+        '''
+        React to message. Get AUX command(s) in the message and react to it. 
+        Return a message simulating real AUX scope response.
+        '''
+        return b''.join([self.handle_cmd(cmd) for cmd in split_cmds(msg)])
+            
+
+
+
