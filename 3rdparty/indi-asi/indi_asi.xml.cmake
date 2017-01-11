632e8c3416dd901cbeb6c70d7d6328f74a04fb1d
Jasem Mutlaq <mutlaqja@ikarustech.com>
2017-01-11 19:22:30 +0300

    Mark ASI as Multiple-Device-Per-Driver type

--- 3rdparty/indi-asi/indi_asi.xml.cmake
+++ 3rdparty/indi-asi/indi_asi.xml.cmake
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <driversList>
 <devGroup group="CCDs">
-        <device label="ZWO CCD">
+        <device label="ZWO CCD" mdpd="true">
                 <driver name="ZWO CCD">indi_asi_ccd</driver>
                 <version>@ASI_VERSION_MAJOR@.@ASI_VERSION_MINOR@</version>
 	</device>
