f76899287762f935346135ebdf3ade57277f9c0f
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2017-03-16 15:50:48 +0100

    Clean up and streamline build on travis

--- travis-ci/build-3rdparty.sh
+++ travis-ci/build-3rdparty.sh
@@ -8,27 +8,9 @@ if [ ! -z $BUILD_THIRD_PARTY ]; then
   echo "==> BUILD_THIRD_PARTY activated"
   mkdir -p build/3rdparty
   pushd build/3rdparty
-
-#  if [ ${TRAVIS_OS_NAME} == "linux" ] ; then
-#    LIBS="libapogee libfishcamp libfli libqhy libqsi libsbig"
-#  else
-#    LIBS="libfishcamp libqsi"
-#  fi
-
-#  for lib in $LIBS ; do
-#  (
-#    echo "Building $lib ..."
-#    mkdir build_$lib
-#    cd build_$lib
-#    cmake -DCMAKE_INSTALL_PREFIX=/usr/local/ . ../../../3rdparty/$lib
-#    make
-#    sudo make install
-#  )
-#  done
-  
-  cmake -DCMAKE_INSTALL_PREFIX=/usr/local/ -DWITH_MI:OPTION=OFF -DWITH_QHY:OPTION=OFF . ../../3rdparty/
+  cmake -DCMAKE_INSTALL_PREFIX=/usr/local/ . ../../3rdparty/
   sudo make install
-  cmake -DCMAKE_INSTALL_PREFIX=/usr/local/ -DWITH_MI:OPTION=OFF -DWITH_QHY:OPTION=OFF . ../../3rdparty/
+  cmake -DCMAKE_INSTALL_PREFIX=/usr/local/ . ../../3rdparty/
   sudo make install
   popd
 else
