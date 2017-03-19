2363105d83087e83adf86846d192e5e17cd61b19
Jasem Mutlaq <mutlaqja@ikarustech.com>
2017-03-19 11:10:37 +0300

    Update libindi.pc to include updated libs in 1.4.1

--- libindi/libindi.pc.cmake
+++ libindi/libindi.pc.cmake
@@ -1,12 +1,12 @@
 prefix=@CMAKE_INSTALL_PREFIX@
 exec_prefix=@CMAKE_INSTALL_PREFIX@
-libdir=@LIB_DESTINATION@
+libdir=@PKG_CONFIG_LIBDIR@
 includedir=@INCLUDE_INSTALL_DIR@
 
 Name: libindi
 Description: Instrument Neutral Distributed Interface
 URL: http://www.indilib.org/
 Version: @CMAKE_INDI_VERSION_STRING@
-Libs: -L${libdir} -lindi
+Libs: -L${libdir} @PKG_CONFIG_LIBS@
 Cflags: -I${includedir} -I${includedir}/libindi
 
