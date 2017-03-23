dc5308016de03a798f3c37e6bc446e4f2acddb3c
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2017-03-23 15:27:55 +0100

    Try gcc-6

--- travis-ci/deps-linux.sh
+++ travis-ci/deps-linux.sh
@@ -7,8 +7,8 @@ sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get -qq update
 
 # try to use gcc5 for the build
-sudo apt-get -q -y install gcc-5 g++-5
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
+sudo apt-get -q -y install gcc-6 g++-6
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6
 
 sudo apt-get -qq update
 sudo apt-get -q -y install \
