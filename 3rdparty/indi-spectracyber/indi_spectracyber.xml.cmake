cb815b6e3bb4471f54c88906e33e3897a4f77ca0
Jasem Mutlaq <mutlaqja@ikarustech.com>
2017-03-26 00:33:39 +0300

    Use Spectrographis as group name

--- 3rdparty/indi-spectracyber/indi_spectracyber.xml.cmake
+++ 3rdparty/indi-spectracyber/indi_spectracyber.xml.cmake
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <driversList>
-<devGroup group="Receivers">
+<devGroup group="Spectrographs">
 	<device label="SpectraCyber">
 		<driver name='SpectraCyber'>indi_spectracyber</driver>
                 <version>@SPECTRACYBER_VERSION_MAJOR@.@SPECTRACYBER_VERSION_MINOR@</version>
