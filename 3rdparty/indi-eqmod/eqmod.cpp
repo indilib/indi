f22577a593251b0ff18cbfe396dea6829e03c9dd
Rumen G.Bogdanovski <rumen@skyarchive.org>
2017-05-12 11:16:37 +0300

    indi-eqmod: attempt to fix missing clock_gettime() on Elcapitan

--- 3rdparty/indi-eqmod/eqmod.cpp
+++ 3rdparty/indi-eqmod/eqmod.cpp
@@ -46,6 +46,7 @@
 #include "config.h"
 
 #include "skywatcher.h"
+#include "mach_gettime.h"
 
 #include <memory>
 
@@ -205,7 +206,7 @@ EQMod::EQMod()
   lndate.days = utc.tm_mday;
   lndate.months = utc.tm_mon + 1;
   lndate.years = utc.tm_year + 1900;
-  clock_gettime(CLOCK_MONOTONIC, &lastclockupdate);
+  utc_time(&lastclockupdate);
   /* initialize random seed: */
   srand ( time(NULL) );
   // Others
@@ -265,7 +266,7 @@ double EQMod::getJulianDate()
   */
   struct timespec currentclock, diffclock;
   double nsecs;
-  clock_gettime(CLOCK_MONOTONIC, &currentclock);
+  utc_time(&currentclock);
   diffclock.tv_sec = currentclock.tv_sec - lastclockupdate.tv_sec;
   diffclock.tv_nsec = currentclock.tv_nsec - lastclockupdate.tv_nsec;
   while (diffclock.tv_nsec > 1000000000) {
@@ -2608,7 +2609,7 @@ bool EQMod::updateTime(ln_date *lndate_utc, double utc_offset)
    utc.tm_year = lndate.years - 1900;
 
    gettimeofday(&lasttimeupdate, NULL);
-   clock_gettime(CLOCK_MONOTONIC, &lastclockupdate);
+   utc_time(&lastclockupdate);
 
    strftime(utc_time, 32, "%Y-%m-%dT%H:%M:%S", &utc);
 
