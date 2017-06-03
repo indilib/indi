a2c9a98316c12b4e00731ca7b722ab634a750396
Eric Vickery <eric@hobbyboards.com>
2017-06-02 22:25:15 -0600

    Initial attempt at making the Focuser Server

---
+++ 3rdparty/indi-focuser-server/config.h.cmake
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* Define INDI Data Dir */
+#cmakedefine INDI_DATA_DIR "@INDI_DATA_DIR@"
+/* Define Driver version */
+#define FS_VERSION_MAJOR @FS_VERSION_MAJOR@
+#define FS_VERSION_MINOR @FS_VERSION_MINOR@
+
+#endif // CONFIG_H
---
+++ 3rdparty/indi-focuser-server/indi_focuser-server.xml.cmake
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<driversList>
+<devGroup group="Auxiliary">
+        <device label="Focuser Server">
+                <driver name="Focuser Server">indi_focuser_server</driver>
+                <version>0.1</version>
+        </device>
+</devGroup>
+</driversList>
+
