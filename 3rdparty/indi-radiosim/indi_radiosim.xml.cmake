4ac5424b2cfe399b3627ab2839d131b8f21860a5
Jasem Mutlaq <mutlaqja@ikarustech.com>
2017-09-17 13:09:42 +0300

    Use default name

--- 3rdparty/indi-radiosim/indi_radiosim.xml.cmake
+++ 3rdparty/indi-radiosim/indi_radiosim.xml.cmake
@@ -2,7 +2,7 @@
 <driversList>
 <devGroup group="Detectors">
         <device label="RadioSim Receiver" mdpd="true">
-                <driver name="RadioSim simulated dish">indi_radiosim_detector</driver>
+                <driver name="RadioSim Receiver">indi_radiosim_detector</driver>
                 <version>@RADIOSIM_VERSION_MAJOR@.@RADIOSIM_VERSION_MINOR@</version>
 	</device>
 </devGroup>
