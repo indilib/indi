97571c127167951886b1d613079c9112931bb1f6
Jaime <jaime@lunatico.es>
2017-11-23 11:49:48 +0100

    Fixed executable names - now being _focus

--- 3rdparty/indi-armadillo-platypus/indi_armplatfocuser.xml.cmake
+++ 3rdparty/indi-armadillo-platypus/indi_armplatfocuser.xml.cmake
@@ -2,11 +2,11 @@
 <driversList>
 <devGroup group="Focusers">
 	<device label="Armadillo focuser">
-                <driver name="Armadillo focuser">indi_armadillo_focuser</driver>
+                <driver name="Armadillo focuser">indi_armadillo_focus</driver>
                 <version>@ARMADILLO_VERSION_MAJOR@.@ARMADILLO_VERSION_MINOR@</version>
 	</device>
 	<device label="Platypus focuser">
-                <driver name="Platypus focuser">indi_platypus_focuser</driver>
+                <driver name="Platypus focuser">indi_platypus_focus</driver>
                 <version>@ARMADILLO_VERSION_MAJOR@.@ARMADILLO_VERSION_MINOR@</version>
 	</device>
 </devGroup>
