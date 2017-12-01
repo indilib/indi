1af382184585a0e86f7ee4381d0110eb1ca0df0d
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2017-12-01 19:01:37 +0100

    [skip ci] move dependencies to docker image

--- .circleci/deps-linux.sh
+++ .circleci/deps-linux.sh
@@ -2,53 +2,53 @@
 
 set -x -e
 
-apt-get -qq update
+#apt-get -qq update
 
-apt-get -q -y install \
- cdbs \
- dpkg-dev \
- debhelper \
- cmake \
- curl \
- dcraw \
- fakeroot \
- libcurl4-gnutls-dev \
- libboost-dev \
- libboost-regex-dev \
- libcfitsio-dev \
- libftdi-dev \
- libgphoto2-dev \
- libgps-dev \
- libgsl-dev \
- libjpeg-dev \
- libnova-dev \
- libopenal-dev \
- libraw-dev \
- libusb-1.0-0-dev \
- librtlsdr-dev \
- libfftw3-dev \
- zlib1g-dev \
- wget
+#apt-get -q -y install \
+# cdbs \
+# dpkg-dev \
+# debhelper \
+# cmake \
+# curl \
+# dcraw \
+# fakeroot \
+# libcurl4-gnutls-dev \
+# libboost-dev \
+# libboost-regex-dev \
+# libcfitsio-dev \
+# libftdi-dev \
+# libgphoto2-dev \
+# libgps-dev \
+# libgsl-dev \
+# libjpeg-dev \
+# libnova-dev \
+# libopenal-dev \
+# libraw-dev \
+# libusb-1.0-0-dev \
+# librtlsdr-dev \
+# libfftw3-dev \
+# zlib1g-dev \
+# wget
 
-# Install libftdi for MGen driver
-CURDIR="$(pwd)"
-apt-get -q -y install libusb-1.0-0-dev libconfuse-dev python3-all-dev doxygen libboost-test-dev python-all-dev swig g++
-cd ~
-mkdir ftdi_src
-cd ftdi_src
-wget http://archive.ubuntu.com/ubuntu/pool/universe/libf/libftdi1/libftdi1_1.2-5build1.dsc
-wget http://archive.ubuntu.com/ubuntu/pool/universe/libf/libftdi1/libftdi1_1.2.orig.tar.bz2
-wget http://archive.ubuntu.com/ubuntu/pool/universe/libf/libftdi1/libftdi1_1.2-5build1.debian.tar.xz
-dpkg-source -x libftdi1_1.2-5build1.dsc
-cd libftdi1-1.2
-# Don't build the unnecessary packages
-truncate -s 1668 debian/control
-truncate -s 1819 debian/rules
-dpkg-buildpackage -rfakeroot -nc -b -j6 -d -uc -us
-cd ..
-dpkg -i libftdi1-2_1.2-5build1_amd64.deb
-dpkg -i libftdi1-dev_1.2-5build1_amd64.deb
-cd $CURDIR
+## Install libftdi for MGen driver
+#CURDIR="$(pwd)"
+##apt-get -q -y install libconfuse-dev python3-all-dev doxygen libboost-test-dev python-all-dev swig g++
+#cd ~
+#mkdir ftdi_src
+#cd ftdi_src
+#wget http://archive.ubuntu.com/ubuntu/pool/universe/libf/libftdi1/libftdi1_1.2-5build1.dsc
+#wget http://archive.ubuntu.com/ubuntu/pool/universe/libf/libftdi1/libftdi1_1.2.orig.tar.bz2
+#wget http://archive.ubuntu.com/ubuntu/pool/universe/libf/libftdi1/libftdi1_1.2-5build1.debian.tar.xz
+#dpkg-source -x libftdi1_1.2-5build1.dsc
+#cd libftdi1-1.2
+## Don't build the unnecessary packages
+#truncate -s 1668 debian/control
+#truncate -s 1819 debian/rules
+#dpkg-buildpackage -rfakeroot -nc -b -j6 -d -uc -us
+#cd ..
+#dpkg -i libftdi1-2_1.2-5build1_amd64.deb
+#dpkg -i libftdi1-dev_1.2-5build1_amd64.deb
+#cd $CURDIR
 
 if [ ! -z $BUILD_INSTALL_GTEST ]; then
   DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
