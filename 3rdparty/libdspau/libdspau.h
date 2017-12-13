f2d37d5c5bf78dc26924d859915be12c80de9cdf
Ilia Platone <info@iliaplatone.com>
2017-12-13 13:20:49 +0100

    [dspau] fixing build errs (pt3)

--- 3rdparty/libdspau/libdspau.h
+++ 3rdparty/libdspau/libdspau.h
@@ -18,7 +18,14 @@
 
 #ifndef _DSPAU_H
 #define _DSPAU_H
-
+#define min(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+#define max(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
 #ifdef  __cplusplus
 extern "C" {
 #endif
