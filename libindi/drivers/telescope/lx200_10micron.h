1db4ad4cbe1363c6b30283c4e000d26dd9d6c5ee
d33psky <d33psky@users.noreply.github.com>
2017-12-16 13:25:39 +0100

    for travis -Winconsistent-missing-override

--- libindi/drivers/telescope/lx200_10micron.h
+++ libindi/drivers/telescope/lx200_10micron.h
@@ -93,9 +93,9 @@ class LX200_10MICRON : public LX200Generic
     LX200_10MICRON();
     ~LX200_10MICRON() {}
 
-    virtual bool ISNewNumber(const char *dev, const char *name, double values[], char *names[], int n);
-    virtual bool ISNewSwitch(const char *dev, const char *name, ISState *states, char *names[], int n);
-    virtual bool ISNewText(const char *dev, const char *name, char *texts[], char *names[], int n);
+    virtual bool ISNewNumber(const char *dev, const char *name, double values[], char *names[], int n) override;
+    virtual bool ISNewSwitch(const char *dev, const char *name, ISState *states, char *names[], int n) override;
+    virtual bool ISNewText(const char *dev, const char *name, char *texts[], char *names[], int n) override;
 
     virtual const char *getDefaultName() override;
     virtual bool Handshake() override;
