5e7b4426e154e4c8926ddfbbe6c653630c058035
Jasem Mutlaq <mutlaqja@ikarustech.com>
2018-01-04 12:00:57 +0300

    Preliminary work for Televue FocusMaster

---
+++ libindi/drivers/focuser/focusmaster.cpp
@@ -0,0 +1,365 @@
+/*******************************************************************************
+  Copyright(c) 2017 Jasem Mutlaq. All rights reserved.
+
+ Televue FocusMaster Driver.
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Library General Public
+ License version 2 as published by the Free Software Foundation.
+ .
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Library General Public License for more details.
+ .
+ You should have received a copy of the GNU Library General Public License
+ along with this library; see the file COPYING.LIB.  If not, write to
+ the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ Boston, MA 02110-1301, USA.
+*******************************************************************************/
+
+#include "focusmaster.h"
+
+#include <cmath>
+#include <cstring>
+#include <memory>
+#include <unistd.h>
+
+#define POLLMS              1000 /* 1000 ms */
+#define FOCUSMASTER_TIMEOUT 1000 /* 1000 ms */
+#define MAX_FM_BUF          16
+
+#define FOCUS_SETTINGS_TAB "Settings"
+
+// We declare an auto pointer to FocusMaster.
+std::unique_ptr<FocusMaster> focusMaster(new FocusMaster());
+
+void ISPoll(void *p);
+
+void ISGetProperties(const char *dev)
+{
+    focusMaster->ISGetProperties(dev);
+}
+
+void ISNewSwitch(const char *dev, const char *name, ISState *states, char *names[], int n)
+{
+    focusMaster->ISNewSwitch(dev, name, states, names, n);
+}
+
+void ISNewText(const char *dev, const char *name, char *texts[], char *names[], int n)
+{
+    focusMaster->ISNewText(dev, name, texts, names, n);
+}
+
+void ISNewNumber(const char *dev, const char *name, double values[], char *names[], int n)
+{
+    focusMaster->ISNewNumber(dev, name, values, names, n);
+}
+
+void ISNewBLOB(const char *dev, const char *name, int sizes[], int blobsizes[], char *blobs[], char *formats[],
+               char *names[], int n)
+{
+    INDI_UNUSED(dev);
+    INDI_UNUSED(name);
+    INDI_UNUSED(sizes);
+    INDI_UNUSED(blobsizes);
+    INDI_UNUSED(blobs);
+    INDI_UNUSED(formats);
+    INDI_UNUSED(names);
+    INDI_UNUSED(n);
+}
+
+void ISSnoopDevice(XMLEle *root)
+{
+    focusMaster->ISSnoopDevice(root);
+}
+
+FocusMaster::FocusMaster()
+{
+    SetFocuserCapability(FOCUSER_CAN_ABORT);
+    setFocuserConnection(CONNECTION_NONE);
+}
+
+bool FocusMaster::Connect()
+{    
+    handle = hid_open(0x134A, 0x9030, nullptr);
+
+    if (handle == nullptr)
+    {
+        DEBUG(INDI::Logger::DBG_ERROR, "No FocusMaster focuser found.");
+        return false;
+    }
+    else
+    {
+        // N.B. Check here if we have the digital readout gadget.
+
+        // if digital readout
+        //SetFocuserCapability(GetFocuserCapability() | FOCUSER_CAN_REL_MOVE | FOCUSER_CAN_ABS_MOVE);
+
+        SetTimer(POLLMS);
+
+    }
+
+    return (handle != nullptr);
+}
+
+bool FocusMaster::Disconnect()
+{
+    hid_close(handle);
+    hid_exit();
+    return true;
+}
+
+const char *FocusMaster::getDefaultName()
+{
+    return (const char *)"FocusMaster";
+}
+
+bool FocusMaster::initProperties()
+{
+    INDI::Focuser::initProperties();
+
+    // Sync to a particular position
+    IUFillNumber(&SyncN[0], "Ticks", "", "%.f", 0, 100000, 100., 0.);
+    IUFillNumberVector(&SyncNP, SyncN, 1, getDeviceName(), "Sync", "", MAIN_CONTROL_TAB, IP_RW, 0, IPS_IDLE);
+
+    // Full Forward/Reverse Motion
+    IUFillSwitch(&FullMotionS[FOCUS_INWARD], "FULL_INWARD", "Full Inward", ISS_OFF);
+    IUFillSwitch(&FullMotionS[FOCUS_OUTWARD], "FULL_OUTWARD", "Full Outward", ISS_OFF);
+    IUFillSwitchVector(&FullMotionSP, FullMotionS, 2, getDeviceName(), "FULL_MOTION", "Full Motion", MAIN_CONTROL_TAB, IP_RW, ISR_ATMOST1, 0, IPS_IDLE);
+
+    FocusAbsPosN[0].min = SyncN[0].min = 0;
+    FocusAbsPosN[0].max = SyncN[0].max;
+    FocusAbsPosN[0].step = SyncN[0].step;
+    FocusAbsPosN[0].value = 0;
+
+    FocusRelPosN[0].max   = (FocusAbsPosN[0].max - FocusAbsPosN[0].min) / 2;
+    FocusRelPosN[0].step  = FocusRelPosN[0].max / 100.0;
+    FocusRelPosN[0].value = 100;
+
+    addSimulationControl();
+
+    return true;
+}
+
+bool FocusMaster::updateProperties()
+{
+    INDI::Focuser::updateProperties();
+
+    if (isConnected())
+    {
+        defineSwitch(&FullMotionSP);
+        //defineNumber(&SyncNP);
+    }
+    else
+    {
+        deleteProperty(FullMotionSP.name);
+        //deleteProperty(SyncNP.name);
+    }
+
+    return true;
+}
+
+void FocusMaster::TimerHit()
+{
+    if (!isConnected())
+        return;
+
+    //uint32_t currentTicks = 0;
+
+    if (FocusTimerNP.s == IPS_BUSY)
+    {
+        float remaining = CalcTimeLeft(focusMoveStart, focusMoveRequest);
+
+        if (remaining <= 0)
+        {
+            FocusTimerNP.s       = IPS_OK;
+            FocusTimerN[0].value = 0;
+            AbortFocuser();
+        }
+        else
+            FocusTimerN[0].value = remaining * 1000.0;
+
+        IDSetNumber(&FocusTimerNP, nullptr);
+    }
+
+#if 0
+    bool rc = getPosition(&currentTicks);
+
+    if (rc)
+        FocusAbsPosN[0].value = currentTicks;    
+
+    if (FocusAbsPosNP.s == IPS_BUSY || FocusRelPosNP.s == IPS_BUSY)
+    {
+        if (targetPosition == FocusAbsPosN[0].value)
+        {
+            if (FocusRelPosNP.s == IPS_BUSY)
+            {
+                FocusRelPosNP.s = IPS_OK;
+                IDSetNumber(&FocusRelPosNP, nullptr);
+            }
+
+            FocusAbsPosNP.s = IPS_OK;
+            DEBUG(INDI::Logger::DBG_DEBUG, "Focuser reached target position.");
+        }
+    }
+
+    IDSetNumber(&FocusAbsPosNP, nullptr);
+#endif
+
+    SetTimer(POLLMS);
+}
+
+bool FocusMaster::ISNewSwitch(const char *dev, const char *name, ISState *states, char *names[], int n)
+{
+    if (dev != nullptr && strcmp(dev, getDeviceName()) == 0)
+    {
+        // Full Motion
+        if (!strcmp(FullMotionSP.name, name))
+        {
+            IUUpdateSwitch(&FullMotionSP, states, names, n);
+            FocusDirection targetDirection = static_cast<FocusDirection>(IUFindOnSwitchIndex(&FullMotionSP));
+
+            FullMotionSP.s = MoveFocuser(targetDirection, 0, 0);
+            return true;
+        }
+    }
+
+    return INDI::Focuser::ISNewSwitch(dev, name, states, names, n);
+
+}
+bool FocusMaster::ISNewNumber(const char *dev, const char *name, double values[], char *names[], int n)
+{
+    if (dev != nullptr && strcmp(dev, getDeviceName()) == 0)
+    {
+        // Sync
+        if (strcmp(SyncNP.name, name) == 0)
+        {
+            IUUpdateNumber(&SyncNP, values, names, n);
+            if (!sync(SyncN[0].value))
+                SyncNP.s = IPS_ALERT;
+            else
+                SyncNP.s = IPS_OK;
+
+            IDSetNumber(&SyncNP, nullptr);
+            return true;
+        }
+    }
+
+    return INDI::Focuser::ISNewNumber(dev, name, values, names, n);
+}
+
+IPState FocusMaster::MoveFocuser(FocusDirection dir, int speed, uint16_t duration)
+{
+    INDI_UNUSED(speed);
+
+    uint8_t command[MAX_FM_BUF] = {0};
+
+    if (dir == FOCUS_INWARD)
+    {
+        command[0] = 0x31;
+        command[1] = 0x21;
+    }
+    else
+    {
+        command[0] = 0x32;
+        command[1] = 0x22;
+    }
+
+    gettimeofday(&focusMoveStart, nullptr);
+    focusMoveRequest = duration / 1000.0;
+
+    if (duration > 0 && duration <= POLLMS)
+    {
+        usleep(duration * 1000);
+        AbortFocuser();
+        return IPS_OK;
+    }
+
+    return IPS_BUSY;
+}
+
+IPState FocusMaster::MoveAbsFocuser(uint32_t targetTicks)
+{
+    INDI_UNUSED(targetTicks);
+
+    FocusAbsPosNP.s = IPS_BUSY;
+
+    return IPS_BUSY;
+}
+
+IPState FocusMaster::MoveRelFocuser(FocusDirection dir, uint32_t ticks)
+{
+    uint32_t finalTicks = FocusAbsPosN[0].value + (ticks * (dir == FOCUS_INWARD ? -1 : 1));
+
+    return MoveAbsFocuser(finalTicks);
+}
+
+bool FocusMaster::sendCommand(const uint8_t *command, char *response)
+{
+    INDI_UNUSED(response);
+
+    int rc = hid_write(handle, command, 2);
+
+    DEBUGF(INDI::Logger::DBG_DEBUG, "CMD <%#02X %#02X>", command[0], command[1]);
+
+    if (rc < 0)
+    {
+        DEBUGF(INDI::Logger::DBG_ERROR, "<%#02X %#02X>: Error writing to device %s", command[0], command[1], hid_error(handle));
+        return false;
+    }
+
+    return true;
+}
+
+bool FocusMaster::setPosition(uint32_t ticks)
+{
+    INDI_UNUSED(ticks);
+    return false;
+}
+
+bool FocusMaster::getPosition(uint32_t *ticks)
+{
+    INDI_UNUSED(ticks);
+    return false;
+}
+
+bool FocusMaster::AbortFocuser()
+{
+    uint8_t command[MAX_FM_BUF] = {0};
+
+    command[0] = 0x30;
+    command[1] = 0x30;
+
+    DEBUG(INDI::Logger::DBG_DEBUG, "Aborting Focuser...");
+
+    bool rc = sendCommand(command);
+
+    if (rc && FullMotionSP.s == IPS_BUSY)
+    {
+        IUResetSwitch(&FullMotionSP);
+        FullMotionSP.s = IPS_IDLE;
+        IDSetSwitch(&FullMotionSP, nullptr);
+    }
+
+    return rc;
+}
+
+bool FocusMaster::sync(uint32_t ticks)
+{
+    INDI_UNUSED(ticks);
+    return false;
+}
+
+float FocusMaster::CalcTimeLeft(timeval start, float req)
+{
+    double timesince;
+    double timeleft;
+    struct timeval now { 0, 0 };
+    gettimeofday(&now, nullptr);
+
+    timesince = (double)(now.tv_sec * 1000.0 + now.tv_usec / 1000) - (double)(start.tv_sec * 1000.0 + start.tv_usec / 1000);
+    timesince = timesince / 1000;
+    timeleft  = req - timesince;
+    return timeleft;
+}
---
+++ libindi/drivers/focuser/focusmaster.h
@@ -0,0 +1,82 @@
+/*******************************************************************************
+  Copyright(c) 2017 Jasem Mutlaq. All rights reserved.
+
+ Televue FocusMaster Driver.
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Library General Public
+ License version 2 as published by the Free Software Foundation.
+ .
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Library General Public License for more details.
+ .
+ You should have received a copy of the GNU Library General Public License
+ along with this library; see the file COPYING.LIB.  If not, write to
+ the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ Boston, MA 02110-1301, USA.
+*******************************************************************************/
+
+#pragma once
+
+#include "indifocuser.h"
+#include "hidapi.h"
+
+class FocusMaster : public INDI::Focuser
+{
+  public:    
+
+    FocusMaster();
+    virtual ~FocusMaster() = default;
+
+    virtual bool ISNewNumber(const char *dev, const char *name, double values[], char *names[], int n);
+    virtual bool ISNewSwitch(const char *dev, const char *name, ISState *states, char *names[], int n);
+
+    const char *getDefaultName();
+    virtual bool initProperties();
+    virtual bool updateProperties();
+
+    bool Connect();
+    bool Disconnect();
+
+    void TimerHit();
+
+    virtual IPState MoveFocuser(FocusDirection dir, int speed, uint16_t duration);
+    virtual IPState MoveAbsFocuser(uint32_t targetTicks);
+    virtual IPState MoveRelFocuser(FocusDirection dir, uint32_t ticks);
+
+    virtual bool AbortFocuser();
+
+  private:
+    /**
+     * @brief sendCommand Send a command to the focuser and optional read a response
+     * @param command 2-byte unsigned char command.
+     * @param response If nullptr (default) is passed, no response is read. If a valid pointer is passed, the response is read into the buffer up until
+     *        MAX_FM_BUF bytes.
+     * @return True if sending command is successful, false otherwise.
+     */
+    bool sendCommand(const uint8_t *command, char *response = nullptr);
+
+    bool setPosition(uint32_t ticks);
+    bool getPosition(uint32_t *ticks);
+
+    bool sync(uint32_t ticks);
+
+    hid_device *handle { nullptr };
+
+    //uint32_t targetPosition { 0 };
+
+    struct timeval focusMoveStart { 0, 0 };
+    float focusMoveRequest { 0 };
+    float CalcTimeLeft(timeval, float);
+
+    // Sync to a particular position
+    INumber SyncN[1];
+    INumberVectorProperty SyncNP;
+
+    // Full Forward / Reverse
+    ISwitch FullMotionS[2];
+    ISwitchVectorProperty FullMotionSP;
+
+};
