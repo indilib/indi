c7f51dfe3c4f72a30b0589106b750c8c01b19452
azwing <alain@zwingelstein.org>
2018-02-09 19:18:21 +0100

    Longitude setting fix

--- libindi/drivers/telescope/lx200_OnStep.cpp
+++ libindi/drivers/telescope/lx200_OnStep.cpp
@@ -988,9 +988,11 @@ bool LX200_OnStep::updateLocation(double latitude, double longitude, double elev
     if (isSimulation())
         return true;
 
-    double onstep_long = longitude - 360;
+    double onstep_long = longitude;
     if (onstep_long < -180)
         onstep_long += 360;
+    if (onstep_long > 180)
+        onstep_long -= 360;
 
     if (!isSimulation() && setSiteLongitude(PortFD, onstep_long) < 0)
     {
