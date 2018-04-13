a40629f8f266f20017d7364e616f53b96ab86d8e
James Lancaster <deathstalker@gmail.com>
2018-04-13 17:13:19 -0500

    Initial commit of raspi camera using Raspicam (https://www.uco.es/investiga/grupos/ava/node/40) to branch for testing

---
+++ 3rdparty/indi-raspicamera/raspi_camera.xml.cmake
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<driversList>
+<devGroup group="CCDs">
+        <device label="Raspberry Pi Camera" mdpd="true">
+                <driver name="RPi Camera">indi_raspi_camera</driver>
+                <version>@GENERIC_VERSION_MAJOR@.@GENERIC_VERSION_MINOR@</version>
+	</device>
+</devGroup>
+</driversList>
