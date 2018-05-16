41ced951e7f40464da8bd7e807bf4de4ae29e331
James Lancaster <deathstalker@gmail.com>
2018-05-15 20:15:19 -0500

    Revert "Initial commit of raspi camera using Raspicam (https://www.uco.es/investiga/grupos/ava/node/40) to branch for testing"
    This reverts commit a40629f8f266f20017d7364e616f53b96ab86d8e.

--- 3rdparty/indi-raspicamera/raspi_camera.xml.cmake
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<driversList>
-<devGroup group="CCDs">
-        <device label="Raspberry Pi Camera" mdpd="true">
-                <driver name="RPi Camera">indi_raspi_camera</driver>
-                <version>@GENERIC_VERSION_MAJOR@.@GENERIC_VERSION_MINOR@</version>
-	</device>
-</devGroup>
-</driversList>
