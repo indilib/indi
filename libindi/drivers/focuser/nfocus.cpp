38cd61e53c19c722aa24211053a84e5264ffa30b
Wolfgang Reissenberger <sterne-jaeger@t-online.de>
2018-05-27 21:35:27 +0200

    Temperature display corrected - value no longer divided by 10

--- libindi/drivers/focuser/nfocus.cpp
+++ libindi/drivers/focuser/nfocus.cpp
@@ -315,7 +315,7 @@ int NFocus::updateNFTemperature(double *value)
     if (sscanf(rf_cmd, "%6f", &temp) == -888)
         return -1;
 
-    *value = (double)temp / 10.;
+    *value = (double)temp;
 
     return 0;
 }
