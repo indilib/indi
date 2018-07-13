dfe20c64a8e2a4221d75b98d0bc6985d8a0851a7
Dirk Niggemann <dirk.niggemann@gmail.com>
2018-07-13 19:31:20 +0100

    Add Nightscape 8300

---
+++ 3rdparty/indi-nightscape/nightscape.h
@@ -0,0 +1,101 @@
+/*
+   INDI Developers Manual
+   Tutorial #3
+
+   "Simple CCD Driver"
+
+   We develop a simple CCD driver.
+
+   Refer to README, which contains instruction on how to build this driver, and use it
+   with an INDI-compatible client.
+
+*/
+
+/** \file simpleccd.h
+    \brief Construct a basic INDI CCD device that simulates exposure & temperature settings. It also generates a random pattern and uploads it as a FITS file.
+    \author Jasem Mutlaq
+
+    \example simpleccd.h
+    A simple CCD device that can capture images and control temperature. It returns a FITS image to the client. To build drivers for complex CCDs, please
+    refer to the INDI Generic CCD driver template in INDI SVN (under 3rdparty).
+*/
+
+#pragma once
+
+#include "indiccd.h"
+#include "nsmsg.h"
+#include "nschannel.h"
+#include "nsdownload.h"
+#include "nsstatus.h"
+#include "nschannel-u.h"
+#include "nschannel-ftd.h"
+#include "nschannel-ser.h"
+
+
+class NightscapeCCD : public INDI::CCD
+{
+  public:
+    NightscapeCCD() = default;
+		virtual bool ISNewSwitch (const char *dev, const char *name, ISState *states, char *names[], int n) override;
+    virtual bool ISNewNumber(const char *dev, const char *name, double values[], char *names[], int n) override;
+
+  protected:
+    // General device functions
+    bool Connect() override;
+    bool Disconnect() override;
+    const char *getDefaultName() override;
+    bool initProperties() override;
+    bool updateProperties() override;
+
+    // CCD specific functions
+    bool StartExposure(float duration) override;
+    bool AbortExposure() override;
+    int SetTemperature(double temperature) override;
+    void TimerHit() override;
+    bool saveConfigItems(FILE *fp) override;
+
+	  
+	  ISwitch  CoolerS [2];
+	  ISwitchVectorProperty CoolerSP;
+	   
+		ISwitch	FanS[3];
+		ISwitchVectorProperty FanSP;
+		
+		INumber CamNumN[1];
+    INumberVectorProperty CamNumNP;
+
+ 		ISwitch  D2xxS [3];
+	  ISwitchVectorProperty D2xxSP;
+	   
+  private:
+    // Utility functions
+    float CalcTimeLeft();
+    void setupParams();
+    void grabImage();
+
+    // Are we exposing?
+    bool InExposure { false };
+    bool InReadout { false };
+    bool InDownload { false };
+    int stat {0};
+    int oldstat { 0};
+    // Struct to keep timing
+    struct timeval ExpStart { 0, 0 };
+
+    float ExposureRequest { 0 };
+    float TemperatureRequest { 0 };
+    Nsmsg * m;
+    NsChannel * cn;
+    NsDownload * dn;
+    NsStatus * st;
+    int fanspeed {3 };
+    int camnum { 1};
+
+    bool cooler { true };
+    float setTemp;
+    int useD2xx { 1 };
+    bool bayer { true };
+    bool dark { false };
+    int ntemps {0};
+    int backoffs {1};
+};
