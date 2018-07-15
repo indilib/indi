acf3081364ffc58aac3f7613859472d50a1b31a6
Robert Lancaster <rlancaste@gmail.com>
2018-07-15 00:35:47 -0400

    Simplifying logic for finding FFMPEG and hopefully making it optional depending on version

--- cmake_modules/FindFFmpeg.cmake
+++ cmake_modules/FindFFmpeg.cmake
@@ -20,62 +20,73 @@
 #
 
 # required ffmpeg library versions, Requiring at least FFMPEG 3.2.11, Hypatia
-set(_avcodec_ver ">=57.64.101" REQUIRED)
-set(_avdevice_ver ">=57.1.100" REQUIRED)
-set(_avformat_ver ">=57.56.100" REQUIRED)
-set(_avutil_ver ">=55.34.100" REQUIRED)
-set(_swscale_ver ">=4.2.100" REQUIRED)
-
- set(FFMPEG_PKGS libavcodec${_avcodec_ver}
-                 libavdevice${_avdevice_ver}
-                 libavformat${_avformat_ver}
-                 libavutil${_avutil_ver}
-                 libswscale${_swscale_ver})
+set(_avcodec_ver ">=57.64.101")
+set(_avdevice_ver ">=57.1.100")
+set(_avformat_ver ">=57.56.100")
+set(_avutil_ver ">=55.34.100")
+set(_swscale_ver ">=4.2.100")
 
 if (FFMPEG_LIBRARIES AND FFMPEG_INCLUDE_DIR)
+
 # in cache already
 set(FFMPEG_FOUND TRUE)
+
 else (FFMPEG_LIBRARIES AND FFMPEG_INCLUDE_DIR)
 # use pkg-config to get the directories and then use these values
 # in the FIND_PATH() and FIND_LIBRARY() calls
+
 find_package(PkgConfig)
 if (PKG_CONFIG_FOUND)
-pkg_check_modules(_FFMPEG_AVCODEC ${FFMPEG_PKGS})
+pkg_check_modules(AVCODEC libavcodec${_avcodec_ver})
+pkg_check_modules(AVDEVICE libavdevice${_avdevice_ver})
+pkg_check_modules(AVFORMAT libavformat${_avformat_ver})
+pkg_check_modules(AVUTIL libavutil${_avutil_ver})
+pkg_check_modules(SWSCALE libswscale${_swscale_ver})
+
 endif (PKG_CONFIG_FOUND)
 
 find_path(FFMPEG_AVCODEC_INCLUDE_DIR
 NAMES libavcodec/avcodec.h
-PATHS ${_FFMPEG_AVCODEC_INCLUDE_DIRS} /usr/include /usr/local/include /opt/local/include /sw/include
+PATHS ${AVCODEC_INCLUDE_DIRS} /usr/include /usr/local/include /opt/local/include /sw/include
 PATH_SUFFIXES ffmpeg libav
 )
 
 find_library(FFMPEG_LIBAVCODEC
 NAMES avcodec libavcodec
-PATHS ${_FFMPEG_AVCODEC_LIBRARY_DIRS} /usr/lib /usr/local/lib /opt/local/lib /sw/lib
+PATHS ${AVCODEC_LIBRARY_DIRS} /usr/lib /usr/local/lib /opt/local/lib /sw/lib
 )
 
 find_library(FFMPEG_LIBAVDEVICE
 NAMES avdevice libavdevice
-PATHS ${_FFMPEG_AVDEVICE_LIBRARY_DIRS} /usr/lib /usr/local/lib /opt/local/lib /sw/lib
+PATHS ${AVDEVICE_LIBRARY_DIRS} /usr/lib /usr/local/lib /opt/local/lib /sw/lib
 )
 
 find_library(FFMPEG_LIBAVFORMAT
 NAMES avformat libavformat
-PATHS ${_FFMPEG_AVFORMAT_LIBRARY_DIRS} /usr/lib /usr/local/lib /opt/local/lib /sw/lib
+PATHS ${AVFORMAT_LIBRARY_DIRS} /usr/lib /usr/local/lib /opt/local/lib /sw/lib
 )
 
 find_library(FFMPEG_LIBAVUTIL
 NAMES avutil libavutil
-PATHS ${_FFMPEG_AVUTIL_LIBRARY_DIRS} /usr/lib /usr/local/lib /opt/local/lib /sw/lib
+PATHS ${AVUTIL_LIBRARY_DIRS} /usr/lib /usr/local/lib /opt/local/lib /sw/lib
 )
 
 find_library(FFMPEG_LIBSWSCALE
 NAMES swscale libswscale
-PATHS ${_FFMPEG_SWSCALE_LIBRARY_DIRS} /usr/lib /usr/local/lib /opt/local/lib /sw/lib
+PATHS ${SWSCALE_LIBRARY_DIRS} /usr/lib /usr/local/lib /opt/local/lib /sw/lib
 )
 
-
-if (FFMPEG_LIBAVCODEC AND FFMPEG_LIBAVDEVICE AND FFMPEG_LIBAVFORMAT AND FFMPEG_LIBAVUTIL AND FFMPEG_LIBSWSCALE)
+#Only set FFMPEG to found if all the libraries are found in the right versions.
+if(AVCODEC_VERSION AND
+                 AVDEVICE_VERSION AND
+                 AVFORMAT_VERSION AND
+                 AVUTIL_VERSION AND
+                 SWSCALE_VERSION AND
+				 FFMPEG_LIBAVCODEC AND
+				 FFMPEG_LIBAVDEVICE AND
+				 FFMPEG_LIBAVFORMAT AND
+				 FFMPEG_LIBAVUTIL AND
+				 FFMPEG_LIBSWSCALE)
 set(FFMPEG_FOUND TRUE)
 endif()
 
