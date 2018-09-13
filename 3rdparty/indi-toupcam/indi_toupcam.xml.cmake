b06f415e2d5ba808e8ddb89d1d8c893270bd1231
Jasem Mutlaq <mutlaqja@ikarustech.com>
2018-09-13 21:31:56 +0300

    Fix name for driver

--- 3rdparty/indi-toupcam/indi_toupcam.xml.cmake
+++ 3rdparty/indi-toupcam/indi_toupcam.xml.cmake
@@ -1,8 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <driversList>
 <devGroup group="CCDs">
-        <device label="TOUPCAM" mdpd="true">
-                <driver name="TOUPCAM">indi_toupcam_ccd</driver>
+        <device label="ToupCam" mdpd="true">
+                <driver name="ToupCam">indi_toupcam_ccd</driver>
                 <version>@TOUPCAM_VERSION_MAJOR@.@TOUPCAM_VERSION_MINOR@</version>
 	</device>
 </devGroup>
