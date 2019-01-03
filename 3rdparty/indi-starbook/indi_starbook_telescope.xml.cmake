d6aaffc25860210408c5a48a2a77883592b16765
Jasem Mutlaq <mutlaqja@ikarustech.com>
2019-01-03 17:47:24 +0300

    Fix typo

--- 3rdparty/indi-starbook/indi_starbook_telescope.xml.cmake
+++ 3rdparty/indi-starbook/indi_starbook_telescope.xml.cmake
@@ -1,9 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<driverslist>
-    <devgroup group="Telescopes">
+<driversList>
+    <devGroup group="Telescopes">
         <device label="Starbook" manufacturer="Vixen">
             <driver name="Starbook">indi_starbook_telescope</driver>
             <version>@STARBOOK_DRIVER_VERSION_MAJOR@.@STARBOOK_DRIVER_VERSION_MINOR@</version>
         </device>
-    </devgroup>
-</driverslist>
+    </devGroup>
+</driversList>
