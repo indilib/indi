98f05db8760b4e7d8f3fef19d5c1c3373cbc2f44
Michael Fulbright <mike.fulbright@pobox.com>
2019-02-05 20:42:24 +0000

    Need to advertise GUIDER_INTERFACE so PHD2 will accept mount

--- libindi/drivers/telescope/pmc8.cpp
+++ libindi/drivers/telescope/pmc8.cpp
@@ -115,7 +115,6 @@ bool PMC8::initProperties()
     IUFillSwitchVector(&MountTypeSP, MountTypeS, 3, getDeviceName(), "MOUNT_TYPE", "Mount Type", CONNECTION_TAB,
 		 IP_RW, ISR_1OFMANY, 0, IPS_IDLE);
 
-
     /* Tracking Mode */
     AddTrackMode("TRACK_SIDEREAL", "Sidereal", true);
     AddTrackMode("TRACK_SOLAR", "Solar");
@@ -151,6 +150,9 @@ bool PMC8::initProperties()
 
     IUFillText(&FirmwareT[0], "Version", "Version", "");
     IUFillTextVector(&FirmwareTP, FirmwareT, 1, getDeviceName(), "Firmware", "Firmware", MAIN_CONTROL_TAB, IP_RO, 0, IPS_IDLE);
+
+    // needed so PHD2 will recognize mount
+    setDriverInterface(getDriverInterface() | GUIDER_INTERFACE);
 
     return true;
 }
