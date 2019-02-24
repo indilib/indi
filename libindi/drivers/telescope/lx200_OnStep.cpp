a935155e769bed370268df8fb5f7a8d75369a96b
azwing <alain@zwingelstein.org>
2019-02-24 09:50:15 +0100

    Fixed Typos in Align

--- libindi/drivers/telescope/lx200_OnStep.cpp
+++ libindi/drivers/telescope/lx200_OnStep.cpp
@@ -2062,10 +2062,10 @@ bool LX200_OnStep::UpdateAlignErr()
 	
     long azmCor = strtold(read_buffer, nullptr);
     fs_sexa(sexabuf, (double)azmCor/3600, 4, 3600);
-    snprintf(polar_error, sizeof(polar_error), "%ld' /%s", azmCor, sexabuf);
+    snprintf(polar_error, sizeof(polar_error), "%ld'' /%s", azmCor, sexabuf);   //azwing typo minutes ' > second ''
     IUSaveText(&OSNAlignErrT[1],polar_error);
     fs_sexa(sexabuf, (double)altCor/3600, 4, 3600);
-    snprintf(polar_error, sizeof(polar_error), "%ld' /%s", altCor, sexabuf);
+    snprintf(polar_error, sizeof(polar_error), "%ld'' /%s", altCor, sexabuf);   //azwing typo minutes ' > second ''
     IUSaveText(&OSNAlignErrT[0],polar_error);
     IDSetText(&OSNAlignErrTP, nullptr);
 	
