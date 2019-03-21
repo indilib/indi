713bda26f664a5ce2bc773ebcb45a3f67fb35b7e
Pedro Rodriguez Perez <pedrodper@gmail.com>
2019-03-21 21:29:09 +0100

    created basis TLE upload skeleton

--- libindi/drivers/telescope/lx200_10micron.h
+++ libindi/drivers/telescope/lx200_10micron.h
@@ -106,6 +106,7 @@ class LX200_10MICRON : public LX200Generic
     bool UnPark() override;
     bool SyncConfigBehaviour(bool cmcfg);
     bool setLocalDate(uint8_t days, uint8_t months, uint16_t years) override;
+    bool setTLEtoFollow(const char *tle);
 
     int AddSyncPoint(double MRa, double MDec, double MSide, double PRa, double PDec, double SidTime);
     int AddSyncPointHere(double PRa, double PDec);
