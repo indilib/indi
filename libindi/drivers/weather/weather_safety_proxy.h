e8ef4385ce23533883589eded71f12eacb2799cf
d33psky <hans@lambermont.dyndns.org>
2019-03-25 17:31:50 +0100

    Update weather_safety_proxy.h
    add missing override

--- libindi/drivers/weather/weather_safety_proxy.h
+++ libindi/drivers/weather/weather_safety_proxy.h
@@ -71,7 +71,7 @@ class WeatherSafetyProxy : public INDI::Weather
 
     virtual bool initProperties() override;
     virtual bool updateProperties() override;
-    virtual void ISGetProperties(const char *dev);
+    virtual void ISGetProperties(const char *dev) override;
     virtual bool ISNewNumber(const char *dev, const char *name, double values[], char *names[], int n) override;
     virtual bool ISNewText(const char *dev, const char *name, char *texts[], char *names[], int n) override;
     virtual bool ISNewSwitch(const char *dev, const char *name, ISState *states, char *names[], int n) override;
