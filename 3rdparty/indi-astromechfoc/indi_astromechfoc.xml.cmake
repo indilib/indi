9600243c9da624602fbf77d8d3ee6ff05dd1ad0a
Jasem Mutlaq <mutlaqja@ikarustech.com>
2019-05-07 09:46:55 +0300

    Correct driver name

--- 3rdparty/indi-astromechfoc/indi_astromechfoc.xml.cmake
+++ 3rdparty/indi-astromechfoc/indi_astromechfoc.xml.cmake
@@ -1,8 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <driversList>
 <devGroup group="Focuser">
-    <device label="Astromechanics INDI Focuser">
-        <driver name="Astromech Focuser">indi_astromechfoc</driver>
+    <device label="Astromechanics FOC">
+        <driver name="Astromechanics FOC">indi_astromechfoc</driver>
         <version>@INDI_ASTROMECHFOC_VERSION_MAJOR@.@INDI_ASTROMECHFOC_VERSION_MINOR@</version>
     </device>
 </devGroup>
