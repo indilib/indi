c14bbc84c83290a57c127f4c4bc150022d9af3f1
Jasem Mutlaq <mutlaqja@ikarustech.com>
2019-09-28 11:25:45 +0300

    Looks like controller might be returning junk.. set just temperature to alert and continue

--- drivers/focuser/focuslynxbase.cpp
+++ drivers/focuser/focuslynxbase.cpp
@@ -1150,18 +1150,10 @@ bool FocusLynxBase::getFocusStatus()
         }
         else
         {
-            char np[8] = {0};
-            int rc = sscanf(response, "%16[^=]= %s", key, np);
-
-            // JM 2019-09-28: With a sample HSM20 with FocusLynx controller, we get -@1.6 which is invalid.
-            if (rc != 2 || (strcmp(np, "NP") && strstr(np, "@") == nullptr))
+            if (TemperatureNP.s != IPS_ALERT)
             {
-                if (TemperatureNP.s != IPS_ALERT)
-                {
-                    TemperatureNP.s = IPS_ALERT;
-                    IDSetNumber(&TemperatureNP, nullptr);
-                }
-                return false;
+                TemperatureNP.s = IPS_ALERT;
+                IDSetNumber(&TemperatureNP, nullptr);
             }
         }
 
