e1ee8dfc5f7d95812b771628bcdcfa4fe60ec292
Jasem Mutlaq <mutlaqja@ikarustech.com>
2019-09-29 15:27:02 +0300

    Only define custom rate if we support it

--- drivers/telescope/lx200telescope.cpp
+++ drivers/telescope/lx200telescope.cpp
@@ -91,7 +91,9 @@ bool LX200Telescope::initProperties()
     AddTrackMode("TRACK_SIDEREAL", "Sidereal", true);
     AddTrackMode("TRACK_SOLAR", "Solar");
     AddTrackMode("TRACK_LUNAR", "Lunar");
-    AddTrackMode("TRACK_CUSTOM", "Custom");
+
+    if (GetTelescopeCapability() & TELESCOPE_HAS_TRACK_RATE)
+        AddTrackMode("TRACK_CUSTOM", "Custom");
 
     if (genericCapability & LX200_HAS_PRECISE_TRACKING_FREQ)
     {
