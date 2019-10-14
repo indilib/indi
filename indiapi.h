7cdcb5f19a2ea815ebcd4288bdbbc1b4efac8704
Jasem Mutlaq <mutlaqja@ikarustech.com>
2019-10-14 07:18:34 +0300

    Bump to v1.8.2

--- indiapi.h
+++ indiapi.h
@@ -133,7 +133,7 @@ For a full list of contributors, please check <a href="https://github.com/indili
 /* INDI Library version */
 #define INDI_VERSION_MAJOR   1
 #define INDI_VERSION_MINOR   8
-#define INDI_VERSION_RELEASE 1
+#define INDI_VERSION_RELEASE 2
 
 /*******************************************************************************
  * Manifest constants
