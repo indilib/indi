f97ff24109761433686ef48e93d28c0d341bf878
Justin Husted <sigstop@gmail.com>
2020-03-24 14:00:29 -0700

    Update copyright notice.

--- drivers/filter_wheel/xagyl_wheel.h
+++ drivers/filter_wheel/xagyl_wheel.h
@@ -1,5 +1,6 @@
 /*******************************************************************************
   Copyright(c) 2020 Jasem Mutlaq. All rights reserved.
+  Copyright(c) 2020 Justin Husted.
 
  This library is free software; you can redistribute it and/or
  modify it under the terms of the GNU Library General Public
