faeae117179ca20ee3a3262dcc5bd36f13fd5731
Pawel Soja <pawel.soja@egzotech.com>
2020-11-07 15:10:53 +0100

    Compatibility fixed - the new firmware has more arguments.

--- drivers/focuser/steeldrive2.cpp
+++ drivers/focuser/steeldrive2.cpp
@@ -563,7 +563,7 @@ bool SteelDriveII::getSummary()
         return false;
 
     std::vector<std::string> params = split(res, ";");
-    if (params.size() != 10)
+    if (params.size() < 10)
         return false;
 
     for (int i = 0; i < 10; i++)
