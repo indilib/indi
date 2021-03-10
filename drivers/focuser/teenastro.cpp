42b40ab21bd028259f27475f0d577f1183817ff4
Markus Noga <ml@noga.de>
2021-03-10 18:40:32 +0100

    copied onfocus driver to create mockup for teenastro focuser driver. buildable and runnable. functionality still 100% identical to onfocus

---
+++ drivers/focuser/teenastro.cpp
@@ -0,0 +1,499 @@
+/*
+    TeenAstro Focuser
+    Copyright (C) 2021 Markus Noga
+    Derived from the below, and hereby made available under the same license.
+
+    TeenAstroFocuser Focuser
+    Copyright (C) 2018 Paul de Backer (74.0632@gmail.com)
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+#include "teenastro_focus.h"
+#include "indicom.h"
+
+#include <stdio.h>
+#include <termios.h>
+#include <string.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <math.h>
+#include <memory>
+
+#define TEENASTRO_FOCUSER_TIMEOUT 4
+
+#define POLLMS_OVERRIDE  1500
+
+std::unique_ptr<TeenAstroFocuser> teenAstroFocuser(new TeenAstroFocuser());
+
+void ISGetProperties(const char *dev)
+{
+         teenAstroFocuser->ISGetProperties(dev);
+}
+
+void ISNewSwitch(const char *dev, const char *name, ISState *states, char *names[], int num)
+{
+         teenAstroFocuser->ISNewSwitch(dev, name, states, names, num);
+}
+
+void ISNewText(	const char *dev, const char *name, char *texts[], char *names[], int num)
+{
+         teenAstroFocuser->ISNewText(dev, name, texts, names, num);
+}
+
+void ISNewNumber(const char *dev, const char *name, double values[], char *names[], int num)
+{
+         teenAstroFocuser->ISNewNumber(dev, name, values, names, num);
+}
+
+void ISNewBLOB (const char *dev, const char *name, int sizes[], int blobsizes[], char *blobs[], char *formats[], char *names[], int n)
+{
+   INDI_UNUSED(dev);
+   INDI_UNUSED(name);
+   INDI_UNUSED(sizes);
+   INDI_UNUSED(blobsizes);
+   INDI_UNUSED(blobs);
+   INDI_UNUSED(formats);
+   INDI_UNUSED(names);
+   INDI_UNUSED(n);
+}
+
+void ISSnoopDevice (XMLEle *root)
+{
+     teenAstroFocuser->ISSnoopDevice(root);
+}
+
+TeenAstroFocuser::TeenAstroFocuser()
+{
+FI::SetCapability(FOCUSER_CAN_ABS_MOVE | FOCUSER_CAN_REL_MOVE | FOCUSER_CAN_ABORT);
+    lastPos = 0;
+}
+
+TeenAstroFocuser::~TeenAstroFocuser()
+{
+}
+
+bool TeenAstroFocuser::initProperties()
+{
+    INDI::Focuser::initProperties();
+    // SetZero
+    IUFillSwitch(&SetZeroS[0], "SETZERO", "Set Current Position to 0", ISS_OFF);
+    IUFillSwitchVector(&SetZeroSP, SetZeroS, 1, getDeviceName(), "Zero Position", "", OPTIONS_TAB, IP_RW, ISR_1OFMANY, 0, IPS_IDLE);
+    // Maximum Travel
+    IUFillNumber(&MaxPosN[0], "MAXPOS", "Maximum Out Position", "%8.0f", 1., 10000000., 0, 0);
+    IUFillNumberVector(&MaxPosNP, MaxPosN, 1, getDeviceName(), "FOCUS_MAXPOS", "Position", OPTIONS_TAB, IP_RW, 0, IPS_IDLE );
+
+
+    /* Relative and absolute movement */
+    FocusRelPosN[0].min = 0.;
+    FocusRelPosN[0].max = 200.;
+    FocusRelPosN[0].value = 0.;
+    FocusRelPosN[0].step = 10.;
+
+    FocusAbsPosN[0].min = 0.;
+    FocusAbsPosN[0].max = 10000000.;
+    FocusAbsPosN[0].value = 0.;
+    FocusAbsPosN[0].step = 500.;
+
+    addDebugControl();
+
+    return true;
+
+}
+
+bool TeenAstroFocuser::updateProperties()
+{
+    INDI::Focuser::updateProperties();
+    if (isConnected())
+    {
+        defineProperty(&MaxPosNP);
+        defineProperty(&SetZeroSP);
+        GetFocusParams();
+        loadConfig(true);
+
+        DEBUG(INDI::Logger::DBG_SESSION, "TeenAstroFocuser parameters updated, focuser ready for use.");
+    }
+    else
+    {
+        deleteProperty(MaxPosNP.name);
+        deleteProperty(SetZeroSP.name);
+    }
+
+    return true;
+
+}
+
+bool TeenAstroFocuser::Handshake()
+{
+    if (Ack())
+    {
+        DEBUG(INDI::Logger::DBG_SESSION, "TeenAstroFocuser is online. Getting focus parameters...");
+        return true;
+    }
+    return false;
+}
+
+const char * TeenAstroFocuser::getDefaultName()
+{
+    return "TeenAstroFocuser";
+}
+
+bool TeenAstroFocuser::Ack()
+{
+    int nbytes_written=0, nbytes_read=0, rc=-1;
+    char errstr[MAXRBUF];
+    char resp[16];
+    sleep(2);
+    tcflush(PortFD, TCIOFLUSH);
+
+    if ( (rc = tty_write(PortFD, ":IP#", 4, &nbytes_written)) != TTY_OK)
+    {
+        tty_error_msg(rc, errstr, MAXRBUF);
+        DEBUGF(INDI::Logger::DBG_ERROR, "Init error: %s.", errstr);
+        return false;
+    }
+    if ( (rc = tty_read(PortFD, resp, 9, TEENASTRO_FOCUSER_TIMEOUT * 2, &nbytes_read)) != TTY_OK)
+    {
+        tty_error_msg(rc, errstr, MAXRBUF);
+        DEBUGF(INDI::Logger::DBG_ERROR, "Init error: %s.", errstr);
+        return false;
+    }
+    tcflush(PortFD, TCIOFLUSH);
+    resp[nbytes_read]='\0';
+    if (!strcmp(resp, "On-Focus#"))
+    { 
+        return true;
+    }
+    else
+    {
+        DEBUGF(INDI::Logger::DBG_ERROR, "Ack Response: %s", resp);
+        return false;
+    }
+}
+
+
+bool TeenAstroFocuser::updatePosition()
+{
+    int nbytes_written=0, nbytes_read=0, rc=-1;
+    char errstr[MAXRBUF];
+    char resp[16];
+    int pos=-1;
+
+    tcflush(PortFD, TCIOFLUSH);
+
+    if ( (rc = tty_write(PortFD, ":GP#", 4, &nbytes_written)) != TTY_OK)
+    {
+        tty_error_msg(rc, errstr, MAXRBUF);
+        DEBUGF(INDI::Logger::DBG_ERROR, "updatePosition error: %s.", errstr);
+        return false;
+    }
+
+    if ( (rc = tty_read_section(PortFD, resp, '#', TEENASTRO_FOCUSER_TIMEOUT, &nbytes_read)) != TTY_OK)
+    {
+        tty_error_msg(rc, errstr, MAXRBUF);
+        DEBUGF(INDI::Logger::DBG_ERROR, "updatePosition error: %s.", errstr);
+        return false;
+    }
+
+    tcflush(PortFD, TCIOFLUSH);
+
+    resp[nbytes_read]='\0';
+
+    rc = sscanf(resp, "%d#", &pos);
+
+    if (rc > 0)
+    {
+        FocusAbsPosN[0].value = pos;
+        IDSetNumber(&FocusAbsPosNP, NULL);
+
+    }
+    else
+    {
+        DEBUGF(INDI::Logger::DBG_ERROR, "Unknown error: focuser position value (%s)", resp);
+        return false;
+    }
+
+  return true;
+}
+
+bool TeenAstroFocuser::updateMaxPos()
+{
+    int nbytes_written=0, nbytes_read=0, rc=-1;
+    char errstr[MAXRBUF];
+    char resp[16];
+    long maxposition;
+
+    tcflush(PortFD, TCIOFLUSH);
+
+    if ( (rc = tty_write(PortFD, ":GM#", 4, &nbytes_written)) != TTY_OK)
+    {
+        tty_error_msg(rc, errstr, MAXRBUF);
+        DEBUGF(INDI::Logger::DBG_ERROR, "updateMaxPosition error: %s.", errstr);
+        return false;
+    }
+
+    if ( (rc = tty_read_section(PortFD, resp, '#', TEENASTRO_FOCUSER_TIMEOUT, &nbytes_read)) != TTY_OK)
+    {
+        tty_error_msg(rc, errstr, MAXRBUF);
+        DEBUGF(INDI::Logger::DBG_ERROR, "updateMaxPosition error: %s.", errstr);
+        return false;
+    }
+
+    tcflush(PortFD, TCIOFLUSH);
+    resp[nbytes_read]='\0';
+
+    rc = sscanf(resp, "%ld#", &maxposition);
+
+    if (rc > 0)
+    {
+
+        MaxPosN[0].value = maxposition;
+        FocusAbsPosN[0].max = maxposition;
+        IDSetNumber(&FocusAbsPosNP, NULL);
+        IDSetNumber(&MaxPosNP, NULL);
+        return true;
+    }
+
+    DEBUGF(INDI::Logger::DBG_ERROR, "Unknown error: focuser maxposition (%s)", resp);
+    return false;
+}
+
+bool TeenAstroFocuser::isMoving()
+{
+    int nbytes_written=0, nbytes_read=0, rc=-1;
+    char errstr[MAXRBUF];
+    char resp[16];
+
+    tcflush(PortFD, TCIOFLUSH);
+
+    if ( (rc = tty_write(PortFD, ":IS#", 4, &nbytes_written)) != TTY_OK)
+    {
+        tty_error_msg(rc, errstr, MAXRBUF);
+        DEBUGF(INDI::Logger::DBG_ERROR, "isMoving error: %s.", errstr);
+        return false;
+    }
+
+    if ( (rc = tty_read_section(PortFD, resp, '#', TEENASTRO_FOCUSER_TIMEOUT, &nbytes_read)) != TTY_OK)
+    {
+        tty_error_msg(rc, errstr, MAXRBUF);
+        DEBUGF(INDI::Logger::DBG_ERROR, "isMoving error: %s.", errstr);
+        return false;
+    }
+
+    tcflush(PortFD, TCIOFLUSH);
+
+    resp[nbytes_read]='\0';
+    if (!strcmp(resp, "M#"))
+        return true;
+    else if (!strcmp(resp, "S#"))
+        return false;
+    else
+    {
+        DEBUGF(INDI::Logger::DBG_ERROR, "Unknown error: isMoving value (%s)", resp);
+        return false;
+    }
+
+}
+
+bool TeenAstroFocuser::MoveMyFocuser(uint32_t position)
+{
+    int nbytes_written=0, rc=-1;
+    char errstr[MAXRBUF];
+    char cmd[24];
+
+    snprintf(cmd, 24, ":MA%d#", position);
+  
+    // Set Position
+    if ( (rc = tty_write(PortFD, cmd, strlen(cmd), &nbytes_written)) != TTY_OK)
+    {
+        tty_error_msg(rc, errstr, MAXRBUF);
+        DEBUGF(INDI::Logger::DBG_ERROR, "setPosition error: %s.", errstr);
+        return false;
+    }
+    return true;
+}
+
+void TeenAstroFocuser::setZero()
+{
+    int nbytes_written=0, rc=-1;
+    char errstr[MAXRBUF];
+    // Set Zero
+    if ( (rc = tty_write(PortFD, ":SZ#" , 4, &nbytes_written)) != TTY_OK)
+    {
+        tty_error_msg(rc, errstr, MAXRBUF);
+        DEBUGF(INDI::Logger::DBG_ERROR, "set Zero error: %s.", errstr);
+        return;
+    }
+    updateMaxPos();
+    return;
+}
+
+
+bool TeenAstroFocuser::setMaxPos(uint32_t maxPos)
+{
+    int nbytes_written=0, rc=-1;
+    char errstr[MAXRBUF];
+    char cmd[24];
+
+    snprintf(cmd, 24, ":SM%d#", maxPos);
+  
+    // Set Max Out
+    if ( (rc = tty_write(PortFD, cmd, strlen(cmd), &nbytes_written)) != TTY_OK)
+    {
+        tty_error_msg(rc, errstr, MAXRBUF);
+        DEBUGF(INDI::Logger::DBG_ERROR, "setPosition error: %s.", errstr);
+        return false;
+    }
+    updateMaxPos();
+    return true;
+}
+
+bool TeenAstroFocuser::ISNewSwitch (const char *dev, const char *name, ISState *states, char *names[], int n)
+{
+    if(strcmp(dev,getDeviceName())==0)
+    {
+    if (!strcmp(SetZeroSP.name, name))
+        {
+        setZero();
+        IUResetSwitch(&SetZeroSP);
+        SetZeroSP.s = IPS_OK;
+        IDSetSwitch(&SetZeroSP, NULL);
+        return true;
+    }
+        
+    return INDI::Focuser::ISNewSwitch(dev, name, states, names, n);
+    }
+    return false;
+}
+
+bool TeenAstroFocuser::ISNewNumber (const char *dev, const char *name, double values[], char *names[], int n)
+{
+    if(strcmp(dev,getDeviceName())==0)
+    {
+        if (!strcmp (name, MaxPosNP.name))
+        {
+            if (values[0] < FocusAbsPosN[0].value)
+            {
+                DEBUGF(INDI::Logger::DBG_ERROR, "Can't set max position lower than current absolute position ( %8.0f )", FocusAbsPosN[0].value);
+                return false;
+            }
+            IUUpdateNumber(&MaxPosNP, values, names, n);               
+            FocusAbsPosN[0].max = MaxPosN[0].value;
+            setMaxPos(MaxPosN[0].value);
+            MaxPosNP.s = IPS_OK;
+            return true;
+        }
+    }
+
+    return INDI::Focuser::ISNewNumber(dev, name, values, names, n);
+
+}
+
+void TeenAstroFocuser::GetFocusParams ()
+{
+    updatePosition();
+    updateMaxPos();
+}
+
+
+IPState TeenAstroFocuser::MoveAbsFocuser(uint32_t targetTicks)
+{
+    uint32_t targetPos = targetTicks;
+
+    bool rc = false;
+
+    rc = MoveMyFocuser(targetPos);
+
+    if (rc == false)
+        return IPS_ALERT;
+
+    FocusAbsPosNP.s = IPS_BUSY;
+
+    return IPS_BUSY;
+}
+
+IPState TeenAstroFocuser::MoveRelFocuser(FocusDirection dir, uint32_t ticks)
+{
+    uint32_t newPosition=0;
+    bool rc=false;
+
+    if (dir == FOCUS_INWARD)
+        newPosition = uint32_t(FocusAbsPosN[0].value) - ticks;
+    else
+        newPosition = uint32_t(FocusAbsPosN[0].value) + ticks;
+
+    rc = MoveMyFocuser(newPosition);
+
+    if (rc == false)
+        return IPS_ALERT;
+
+    FocusRelPosN[0].value = ticks;
+    FocusRelPosNP.s = IPS_BUSY;
+
+    return IPS_BUSY;
+}
+
+void TeenAstroFocuser::TimerHit()
+{
+    if (isConnected() == false)
+    {
+        SetTimer(POLLMS_OVERRIDE);
+        return;
+    }
+
+    bool rc = updatePosition();
+    if (rc)
+    {
+        if (fabs(lastPos - FocusAbsPosN[0].value) > 5)
+        {
+            IDSetNumber(&FocusAbsPosNP, NULL);
+            lastPos = FocusAbsPosN[0].value;
+        }
+    }
+
+
+    if (FocusAbsPosNP.s == IPS_BUSY || FocusRelPosNP.s == IPS_BUSY)
+    {
+        if (isMoving() == false)
+        {
+            FocusAbsPosNP.s = IPS_OK;
+            FocusRelPosNP.s = IPS_OK;
+            IDSetNumber(&FocusAbsPosNP, NULL);
+            IDSetNumber(&FocusRelPosNP, NULL);
+            lastPos = FocusAbsPosN[0].value;
+            DEBUG(INDI::Logger::DBG_SESSION, "Focuser reached requested position.");
+        }
+    }
+    SetTimer(POLLMS_OVERRIDE);
+
+}
+
+bool TeenAstroFocuser::AbortFocuser()
+{
+    int nbytes_written;
+    if (tty_write(PortFD, ":MH#", 4, &nbytes_written) == TTY_OK)
+    {
+        FocusAbsPosNP.s = IPS_IDLE;
+        FocusRelPosNP.s = IPS_IDLE;
+        IDSetNumber(&FocusAbsPosNP, NULL);
+        IDSetNumber(&FocusRelPosNP, NULL);
+        return true;
+    }
+    else
+        return false;
+}
+
+
---
+++ drivers/focuser/teenastro.h
@@ -0,0 +1,67 @@
+/*
+    TeenAstro Focuser
+    Copyright (C) 2021 Markus Noga
+    Derived from the below, and hereby made available under the same license.
+
+    OnFocus Focuser
+    Copyright (C) 2018 Paul de Backer (74.0632@gmail.com)
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+#ifndef TEENASTRO_FOCUSER_H
+#define TEENASTRO_FOCUSER_H
+
+#include "indibase/indifocuser.h"
+
+class TeenAstroFocuser : public INDI::Focuser
+{
+public:
+    TeenAstroFocuser();
+    ~TeenAstroFocuser();
+
+    virtual bool Handshake();
+    const char * getDefaultName();
+    virtual bool initProperties();
+    virtual bool updateProperties();
+    virtual bool ISNewNumber (const char *dev, const char *name, double values[], char *names[], int n);
+    virtual bool ISNewSwitch (const char *dev, const char *name, ISState *states, char *names[], int n);
+    virtual IPState MoveAbsFocuser(uint32_t ticks);
+    virtual IPState MoveRelFocuser(FocusDirection dir, uint32_t ticks);
+    virtual bool AbortFocuser();
+    virtual void TimerHit();
+
+private:
+
+    double targetPos, lastPos;
+ 
+    void GetFocusParams();
+    void setZero();
+    bool updateMaxPos();
+    bool updateTemperature();
+    bool updatePosition();
+    bool isMoving();
+    bool Ack();
+
+    bool MoveMyFocuser(uint32_t position);
+    bool setMaxPos(uint32_t maxPos);
+
+    INumber MaxPosN[1];
+    INumberVectorProperty MaxPosNP;
+    ISwitch SetZeroS[1];
+    ISwitchVectorProperty SetZeroSP;
+};
+
+#endif // TEENASTRROFOCUSER_H
