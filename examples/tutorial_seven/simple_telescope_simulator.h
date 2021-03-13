d9a637b72ccdd8ee52b630c5a7155dd2a5d13962
Pawel Soja <kernel32.pl@gmail.com>
2021-03-13 12:27:26 +0100

    remove unnecessary ISxxx implementation

--- examples/tutorial_seven/simple_telescope_simulator.h
+++ examples/tutorial_seven/simple_telescope_simulator.h
@@ -18,15 +18,10 @@ private:
     virtual const char *getDefaultName() override;
     virtual bool Goto(double ra, double dec) override;
     virtual bool initProperties() override;
-    friend void ISNewBLOB(const char *dev, const char *name, int sizes[], int blobsizes[], char *blobs[],
-                          char *formats[], char *names[], int n);
     virtual bool ISNewBLOB(const char *dev, const char *name, int sizes[], int blobsizes[], char *blobs[],
                            char *formats[], char *names[], int n) override;
-    friend void ISNewNumber(const char *dev, const char *name, double values[], char *names[], int n);
     virtual bool ISNewNumber(const char *dev, const char *name, double values[], char *names[], int n) override;
-    friend void ISNewSwitch(const char *dev, const char *name, ISState *states, char *names[], int n);
     virtual bool ISNewSwitch(const char *dev, const char *name, ISState *states, char *names[], int n) override;
-    friend void ISNewText(const char *dev, const char *name, char *texts[], char *names[], int n);
     virtual bool ISNewText(const char *dev, const char *name, char *texts[], char *names[], int n) override;
     virtual bool MoveNS(INDI_DIR_NS dir, TelescopeMotionCommand command) override;
     virtual bool MoveWE(INDI_DIR_WE dir, TelescopeMotionCommand command) override;
