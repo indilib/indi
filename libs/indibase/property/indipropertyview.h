c4320ddbc9617b2696c3194eb6a24954826eda4d
Pawel Soja <kernel32.pl@gmail.com>
2021-03-29 00:49:44 +0200

    Fix Wtautological-undefined-compare.

--- libs/indibase/property/indipropertyview.h
+++ libs/indibase/property/indipropertyview.h
@@ -261,7 +261,7 @@ public:
     WidgetView()                                           { memset(this, 0, sizeof(*this)); }
     ~WidgetView()                                          { free(this->text); }
     void clear()                                           { free(this->text); memset(this, 0, sizeof(*this)); }
-    bool isNull() const                                    { return this == nullptr; }
+    bool isNull() const                                    { return reinterpret_cast<const void*>(this) == nullptr; }
 
 public: // setters
     void setParent(ITextVectorProperty *parent)            { this->tvp = parent; }
@@ -311,7 +311,7 @@ public:
     WidgetView()                                           { memset(this, 0, sizeof(*this)); }
     ~WidgetView()                                          { }
     void clear()                                           { memset(this, 0, sizeof(*this)); }
-    bool isNull() const                                    { return this == nullptr; }
+    bool isNull() const                                    { return reinterpret_cast<const void*>(this) == nullptr; }
 
 public: // setters
     void setParent(INumberVectorProperty *parent)          { this->nvp = parent; }
@@ -374,7 +374,7 @@ public:
     WidgetView()                                           { memset(this, 0, sizeof(*this)); }
     ~WidgetView()                                          { }
     void clear()                                           { memset(this, 0, sizeof(*this)); }
-    bool isNull() const                                    { return this == nullptr; }
+    bool isNull() const                                    { return reinterpret_cast<const void*>(this) == nullptr; }
 
 public: // setters
     void setParent(ISwitchVectorProperty *parent)          { this->svp = parent; }
@@ -427,7 +427,7 @@ public:
     WidgetView()                                           { memset(this, 0, sizeof(*this)); }
     ~WidgetView()                                          { }
     void clear()                                           { memset(this, 0, sizeof(*this)); }
-    bool isNull() const                                    { return this == nullptr; }
+    bool isNull() const                                    { return reinterpret_cast<const void*>(this) == nullptr; }
 
 public: // setters
     void setParent(ILightVectorProperty *parent)           { this->lvp = parent; }
@@ -480,7 +480,7 @@ public:
     WidgetView()                                           { memset(this, 0, sizeof(*this)); }
     ~WidgetView()                                          { free(this->blob); }
     void clear()                                           { free(this->blob); memset(this, 0, sizeof(*this)); }
-    bool isNull() const                                    { return this == nullptr; }
+    bool isNull() const                                    { return reinterpret_cast<const void*>(this) == nullptr; }
 
 public: // setters
     void setParent(IBLOBVectorProperty *parent)            { this->bvp = parent; }
