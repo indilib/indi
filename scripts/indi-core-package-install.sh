32fecf7dc4490cccc907543785456ec5978400c3
Pawel Soja <kernel32.pl@gmail.com>
2021-03-29 16:11:07 +0200

    bsdtar support

--- scripts/indi-core-package-install.sh
+++ scripts/indi-core-package-install.sh
@@ -2,4 +2,6 @@
 
 set -e
 
-$(command -v sudo) tar -hxvf packages/indi-core-package.tar --strip-components=1 -C /
+command -v bsdtar >/dev/null 2>&1 && \
+    $(command -v sudo) bsdtar -xvf packages/indi-core-package.tar --strip-components=1 -C / || \
+    $(command -v sudo)   tar -hxvf packages/indi-core-package.tar --strip-components=1 -C /
