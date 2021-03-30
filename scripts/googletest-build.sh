5eba57613a59f1f5706f679dd3ea0e7f9d19b6d0
Pawel Soja <kernel32.pl@gmail.com>
2021-03-30 12:30:16 +0200

    Autodetect jobs for 'make'.

--- scripts/googletest-build.sh
+++ scripts/googletest-build.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+command -v nproc >/dev/null 2>&1 || function nproc {
+    command -v sysctl >/dev/null 2>&1 &&  \
+        sysctl -n hw.logicalcpu ||
+        echo "3"
+}
+
 test -d googletest || git clone https://github.com/google/googletest.git googletest
 
 mkdir -p build/googletest
@@ -11,5 +17,5 @@ cmake \
     -DBUILD_SHARED_LIBS=ON \
     . ../../googletest
 
-make -j3
+make -j$(($(nproc)+1))
 popd
