2270619458827f9e6c082e79915f5ce685f57b2a
Jasem Mutlaq <mutlaqja@ikarustech.com>
2021-05-14 19:47:02 +0300

    Remove unused focuser temperature

--- drivers/auxiliary/pegasus_upb.cpp
+++ drivers/auxiliary/pegasus_upb.cpp
@@ -40,7 +40,7 @@ static std::unique_ptr<PegasusUPB> upb(new PegasusUPB());
 
 PegasusUPB::PegasusUPB() : FI(this), WI(this)
 {
-    setVersion(1, 5);
+    setVersion(1, 6);
 
     lastSensorData.reserve(21);
     lastPowerData.reserve(4);
@@ -327,12 +327,6 @@ bool PegasusUPB::initProperties()
     IUFillNumber(&FocuserSettingsN[SETTING_MAX_SPEED], "SETTING_MAX_SPEED", "Max Speed (%)", "%.f", 0, 900, 100, 400);
     IUFillNumberVector(&FocuserSettingsNP, FocuserSettingsN, 1, getDeviceName(), "FOCUSER_SETTINGS", "Settings", FOCUS_TAB,
                        IP_RW, 60, IPS_IDLE);
-
-    // Temperature
-    IUFillNumber(&FocuserTemperatureN[0], "TEMPERATURE", "Value (C)", "%4.2f", -50, 85, 1, 0);
-    IUFillNumberVector(&FocuserTemperatureNP, FocuserTemperatureN, 1, getDeviceName(), "FOCUS_TEMPERATURE", "Temperature",
-                       FOCUS_TAB, IP_RO, 60, IPS_IDLE);
-
     ////////////////////////////////////////////////////////////////////////////
     /// Firmware Group
     ////////////////////////////////////////////////////////////////////////////
@@ -417,8 +411,6 @@ bool PegasusUPB::updateProperties()
         // Focuser
         FI::updateProperties();
         defineProperty(&FocuserSettingsNP);
-        //defineProperty(&FocusBacklashSP);
-        defineProperty(&FocuserTemperatureNP);
 
         WI::updateProperties();
 
@@ -470,7 +462,6 @@ bool PegasusUPB::updateProperties()
         // Focuser
         FI::updateProperties();
         deleteProperty(FocuserSettingsNP.name);
-        deleteProperty(FocuserTemperatureNP.name);
 
         WI::updateProperties();
 
