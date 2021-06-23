859d93c7d9da276213ab21fe01ae9947422d5f6f
Efstathios Chrysikos <stathis@chrysikos.gr>
2021-06-23 18:04:39 +0300

    Add copyright headers to Pegasus FlatMaster, FocusCube, ScopsOAG Devices

--- drivers/auxiliary/pegasus_flatmaster.cpp
+++ drivers/auxiliary/pegasus_flatmaster.cpp
@@ -1,3 +1,27 @@
+/*******************************************************************************
+  Copyright(c) 2021 Chrysikos Efstathios. All rights reserved.
+
+  Pegasus FlatMaster
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 2 of the License, or (at your option)
+  any later version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+  more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
+
+  The full GNU General Public License is included in this distribution in the
+  file called LICENSE.
+*******************************************************************************/
+
 #include "pegasus_flatmaster.h"
 
 #include "indicom.h"
