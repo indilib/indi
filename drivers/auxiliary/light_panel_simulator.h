a18b69df5c61c24a53322086ba0e73ef14e5e828
Jasem Mutlaq <mutlaqja@ikarustech.com>
2021-10-05 09:43:30 +0300

    Add missing ISGetProperties

--- drivers/auxiliary/light_panel_simulator.h
+++ drivers/auxiliary/light_panel_simulator.h
@@ -1,4 +1,4 @@
-/*******************************************************************************  
+/*******************************************************************************
   Light Panel Simulator
 
   SPDX-FileCopyrightText: 2021 Jasem Mutlaq
@@ -16,6 +16,8 @@ class LightPanelSimulator : public INDI::DefaultDevice, public INDI::LightBoxInt
         LightPanelSimulator();
         virtual ~LightPanelSimulator() override = default;
 
+        void ISGetProperties(const char *dev) override;
+
         bool ISNewNumber(const char *dev, const char *name, double values[], char *names[], int n) override;
         bool ISNewText(const char *dev, const char *name, char *texts[], char *names[], int n) override;
         bool ISNewSwitch(const char *dev, const char *name, ISState *states, char *names[], int n) override;
@@ -25,11 +27,20 @@ class LightPanelSimulator : public INDI::DefaultDevice, public INDI::LightBoxInt
         bool initProperties() override;
         bool updateProperties() override;
 
-        bool Connect() override { return true;}
-        bool Disconnect() override {return true;}
-        const char *getDefaultName() override { return "Light Panel Simulator";}
+        bool Connect() override
+        {
+            return true;
+        }
+        bool Disconnect() override
+        {
+            return true;
+        }
+        const char *getDefaultName() override
+        {
+            return "Light Panel Simulator";
+        }
 
         // From Light Box
         virtual bool SetLightBoxBrightness(uint16_t value) override;
-        virtual bool EnableLightBox(bool enable) override;    
+        virtual bool EnableLightBox(bool enable) override;
 };
