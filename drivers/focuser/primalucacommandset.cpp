bc1623cd52b2d539475a73228c364b0e5664953d
Jasem Mutlaq <mutlaqja@ikarustech.com>
2022-07-29 08:33:23 +0300

    Remove unused fields

--- drivers/focuser/primalucacommandset.cpp
+++ drivers/focuser/primalucacommandset.cpp
@@ -150,7 +150,7 @@ template <typename T> bool Communication::motorCommand(MotorType type, const jso
 /******************************************************************************************************
  * Common Focuser functions between SestoSenso2 & Esatto
 *******************************************************************************************************/
-Focuser::Focuser(const std::string &name, int port) : m_DeviceName(name), m_PortFD(port)
+Focuser::Focuser(const std::string &name, int port)
 {
     m_Communication.reset(new Communication(name, port));
 }
