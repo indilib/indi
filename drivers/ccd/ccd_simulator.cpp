537c7303852e215e44de29613a1888b983936347
Wolfgang Reissenberger <sterne-jaeger@openfuture.de>
2022-11-02 08:47:40 +0100

    Improved star brightness handling for higher gains

--- drivers/ccd/ccd_simulator.cpp
+++ drivers/ccd/ccd_simulator.cpp
@@ -523,8 +523,7 @@ int CCDSim::DrawCcdFrame(INDI::CCDChip * targetChip)
     else
         exposure_time = ExposureRequest;
 
-    auto gain = std::max(10.0, GainN[0].value);
-    exposure_time *= sqrt(gain) / 10.0;
+    exposure_time *= (1 + sqrt(GainN[0].value));
 
     auto targetFocalLength = ScopeInfoNP[FocalLength].getValue() > 0 ? ScopeInfoNP[FocalLength].getValue() : snoopedFocalLength;
 
