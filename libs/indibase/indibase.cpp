7dba8d4135e8bf926f74c186e4709efad35b9e47
Paweł Soja <pawel.soja@egzotech.com>
2022-11-14 23:03:24 +0100

    fix comments, sorting out the code

--- libs/indibase/indibase.cpp
+++ libs/indibase/indibase.cpp
@@ -4,25 +4,13 @@
 namespace INDI
 {
 
-void BaseMediator::newDevice(INDI::BaseDevice *)
-{ }
-
 void BaseMediator::newDevice(INDI::BaseDevice)
-{ }
-
-void BaseMediator::removeDevice(INDI::BaseDevice *)
 { }
 
 void BaseMediator::removeDevice(INDI::BaseDevice)
 { }
 
-void BaseMediator::newProperty(INDI::Property *)
-{ }
-
 void BaseMediator::newProperty(INDI::Property)
-{ }
-
-void BaseMediator::removeProperty(INDI::Property *)
 { }
 
 void BaseMediator::removeProperty(INDI::Property)
@@ -31,31 +19,16 @@ void BaseMediator::removeProperty(INDI::Property)
 void BaseMediator::newBLOB(IBLOB *)
 { }
 
-void BaseMediator::newSwitch(ISwitchVectorProperty *)
-{ }
-
 void BaseMediator::newSwitch(INDI::PropertySwitch)
-{ }
-
-void BaseMediator::newNumber(INumberVectorProperty *)
 { }
 
 void BaseMediator::newNumber(INDI::PropertyNumber)
 { }
 
-void BaseMediator::newText(ITextVectorProperty *)
-{ }
-
 void BaseMediator::newText(INDI::PropertyText)
 { }
 
-void BaseMediator::newLight(ILightVectorProperty *)
-{ }
-
 void BaseMediator::newLight(INDI::PropertyLight)
-{ }
-
-void BaseMediator::newMessage(INDI::BaseDevice *, int)
 { }
 
 void BaseMediator::newMessage(INDI::BaseDevice, int)
@@ -65,6 +38,34 @@ void BaseMediator::serverConnected()
 { }
 
 void BaseMediator::serverDisconnected(int)
+{ }
+
+// deprecated
+void BaseMediator::newDevice(INDI::BaseDevice *)
+{ }
+
+void BaseMediator::removeDevice(INDI::BaseDevice *)
+{ }
+
+void BaseMediator::newProperty(INDI::Property *)
+{ }
+
+void BaseMediator::removeProperty(INDI::Property *)
+{ }
+
+void BaseMediator::newSwitch(ISwitchVectorProperty *)
+{ }
+
+void BaseMediator::newNumber(INumberVectorProperty *)
+{ }
+
+void BaseMediator::newText(ITextVectorProperty *)
+{ }
+
+void BaseMediator::newLight(ILightVectorProperty *)
+{ }
+
+void BaseMediator::newMessage(INDI::BaseDevice *, int)
 { }
 
 }
