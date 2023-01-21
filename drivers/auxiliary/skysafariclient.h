aeb3a289ac81e22053b93f8e94e85988bab187a4
Jasem Mutlaq <mutlaqja@ikarustech.com>
2023-01-21 12:39:16 +0300

    Update SkySafari client to be compatible with INDI v2.0.0

--- drivers/auxiliary/skysafariclient.h
+++ drivers/auxiliary/skysafariclient.h
@@ -88,18 +88,8 @@ class SkySafariClient : public INDI::BaseClient
         bool setTimeUTC();
 
     protected:
-        virtual void newDevice(INDI::BaseDevice *dp);
-        virtual void removeDevice(INDI::BaseDevice */*dp*/) {}
-        virtual void newProperty(INDI::Property *property);
-        virtual void removeProperty(INDI::Property */*property*/) {}
-        virtual void newBLOB(IBLOB */*bp*/) {}
-        virtual void newSwitch(ISwitchVectorProperty */*svp*/) {}
-        virtual void newNumber(INumberVectorProperty */*nvp*/) {}
-        virtual void newMessage(INDI::BaseDevice */*dp*/, int /*messageID*/) {}
-        virtual void newText(ITextVectorProperty */*tvp*/) {}
-        virtual void newLight(ILightVectorProperty */*lvp*/) {}
-        virtual void serverConnected() {}
-        virtual void serverDisconnected(int /*exit_code*/) {}
+        virtual void newDevice(INDI::BaseDevice dp) override;
+        virtual void newProperty(INDI::Property property) override;
 
     private:
         std::string mount;
