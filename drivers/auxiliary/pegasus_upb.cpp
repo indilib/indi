indilib/indi

commit 37860f05cb3e4aa55cf7f0d67968b1bcf579e6c3

Ensure defensive check of UBP previous data to fix crash - fixes #1662 (#1663)

drivers/auxiliary/pegasus_upb.cpp
@@ -1341,11 +1341,26 @@ bool PegasusUPB::sendFirmware()
 //////////////////////////////////////////////////////////////////////
 bool PegasusUPB::sensorUpdated(const std::vector<std::string> &result, uint8_t start, uint8_t end)
 {
-    for (uint8_t index = start; index <= end; index++)
-        if (result[index] != lastSensorData[index])
+    if (lastSensorData.empty())
+        return true;
+
+    for (uint8_t index = start; index <= end; index++){
+        if (index >= lastSensorData.size() or result[index] != lastSensorData[index])
             return true;
+    }
 
     return false;
+}
+
+//////////////////////////////////////////////////////////////////////
+///
+//////////////////////////////////////////////////////////////////////
+bool PegasusUPB::stepperUpdated(const std::vector<std::string> &result, u_int8_t index)
+{
+    if (lastStepperData.empty())
+        return true;
+
+    return index >= lastStepperData.size() or result[index] != lastSensorData[index];
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -1641,13 +1656,13 @@ bool PegasusUPB::getStepperData()
             IDSetNumber(&FocusAbsPosNP, nullptr);
             IDSetNumber(&FocusRelPosNP, nullptr);
         }
-        else if (result[0] != lastStepperData[0])
+        else if (stepperUpdated(result, 0))
             IDSetNumber(&FocusAbsPosNP, nullptr);
 
         FocusReverseS[INDI_ENABLED].s = (std::stoi(result[2]) == 1) ? ISS_ON : ISS_OFF;
         FocusReverseS[INDI_DISABLED].s = (std::stoi(result[2]) == 1) ? ISS_OFF : ISS_ON;
 
-        if (result[2] != lastStepperData[2])
+        if (stepperUpdated(result, 1))
             IDSetSwitch(&FocusReverseSP, nullptr);
 
         uint16_t backlash = std::stoi(result[3]);
@@ -1656,7 +1671,7 @@ bool PegasusUPB::getStepperData()
             FocusBacklashN[0].value = backlash;
             FocusBacklashS[INDI_ENABLED].s = ISS_OFF;
             FocusBacklashS[INDI_DISABLED].s = ISS_ON;
-            if (result[3] != lastStepperData[3])
+            if (stepperUpdated(result, 3))
             {
                 IDSetSwitch(&FocusBacklashSP, nullptr);
                 IDSetNumber(&FocuserSettingsNP, nullptr);
@@ -1667,7 +1682,7 @@ bool PegasusUPB::getStepperData()
             FocusBacklashS[INDI_ENABLED].s = ISS_ON;
             FocusBacklashS[INDI_DISABLED].s = ISS_OFF;
             FocusBacklashN[0].value = backlash;
-            if (result[3] != lastStepperData[3])
+            if (stepperUpdated(result, 3))
             {
                 IDSetSwitch(&FocusBacklashSP, nullptr);
                 IDSetNumber(&FocuserSettingsNP, nullptr);

drivers/auxiliary/pegasus_upb.h
@@ -129,8 +129,19 @@ class PegasusUPB : public INDI::DefaultDevice, public INDI::FocuserInterface, pu
 
         /**
          * @return Return true if sensor data different from last data
+         *
+         * If the previous sensor data is empty then this will always
+         * return true.
          */
         bool sensorUpdated(const std::vector<std::string> &result, uint8_t start, uint8_t end);
+
+        /**
+         * @return Return true if stepper data different from last data.
+         *
+         * If the previous stepper data is empty then this will always
+         * return true.
+         */
+        bool stepperUpdated(const std::vector<std::string> &result, u_int8_t index);
 
         int PortFD { -1 };
         bool setupComplete { false };

commit aab473f8ad33f7683a2e0a96db86e1894aa1715c

drivers: auxiliary: use stdint.h types (fix musl build)
Followup to 7df8ca6d0de4157268de625c88705c2d96349876. This regressed in
37860f05cb3e4aa55cf7f0d67968b1bcf579e6c3 with the addition of non-standard
u_int* types (note the extra underscore).

Bug: https://bugs.gentoo.org/873403
Signed-off-by: Sam James <sam@gentoo.org>

drivers/auxiliary/pegasus_upb.cpp
@@ -1356,7 +1356,7 @@ bool PegasusUPB::sensorUpdated(const std::vector<std::string> &result, uint8_t s
 //////////////////////////////////////////////////////////////////////
 ///
 //////////////////////////////////////////////////////////////////////
-bool PegasusUPB::stepperUpdated(const std::vector<std::string> &result, u_int8_t index)
+bool PegasusUPB::stepperUpdated(const std::vector<std::string> &result, uint8_t index)
 {
     if (lastStepperData.empty())
         return true;

drivers/auxiliary/pegasus_upb.h
@@ -141,7 +141,7 @@ class PegasusUPB : public INDI::DefaultDevice, public INDI::FocuserInterface, pu
          * If the previous stepper data is empty then this will always
          * return true.
          */
-        bool stepperUpdated(const std::vector<std::string> &result, u_int8_t index);
+        bool stepperUpdated(const std::vector<std::string> &result, uint8_t index);
 
         int PortFD { -1 };
         bool setupComplete { false };