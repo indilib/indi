c2b12b3d057f7b7e7996763eefc065f9357a379a
Thomas Klausner <wiz@gatalith.at>
2024-05-03 13:34:47 +0200

    Handle NetBSD like FreeBSD
    Fixes error about missing B576000 symbol.

--- libs/indicore/indicom.c
+++ libs/indicore/indicom.c
@@ -793,7 +793,7 @@ int tty_connect(const char *device, int bit_rate, int word_size, int parity, int
     case 57600:  bps = B57600;  break;
     case 115200: bps = B115200; break;
     case 230400: bps = B230400; break;
-#if !defined(__APPLE__) && !defined(__FreeBSD__)
+#if !defined(__APPLE__) && !defined(__FreeBSD__) && !defined(__NetBSD__)
     case 460800: bps = B460800; break;
     case 576000: bps = B576000; break;
     case 921600: bps = B921600; break;
