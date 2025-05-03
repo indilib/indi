cc5168835c7c74de3239f908117aaf48f3f9d981
Alexey Sokolov <alexey@asokolov.org>
2025-05-03 05:51:39 +0100

    Pass -lindiclient in pkg-config (#2230)
    Potential fix for #2156

--- libindi.pc.cmake
+++ libindi.pc.cmake
@@ -7,7 +7,7 @@ Name: libindi
 Description: Instrument Neutral Distributed Interface
 URL: http://www.indilib.org/
 Version: @CMAKE_INDI_VERSION_STRING@
-Libs: -L${libdir} @PKG_CONFIG_LIBS@
+Libs: -L${libdir} -lindiclient
 Libs.private: -lz -lcfitsio -lnova
 Cflags: -I${includedir} -I${includedir}/libindi
 
