db96a35e7617a4b6a3be19f2d011bffe84a3ef07
WandererAstro <skywatcherwsl2000@gmail.com>
2025-08-04 22:26:51 +0800

    Driver for Wanderer Eclipse added.

--- drivers/auxiliary/wanderer_eclipse.h
+++ drivers/auxiliary/wanderer_eclipse.h
@@ -40,11 +40,10 @@ public:
     virtual ~WandererEclipse() = default;
 
     virtual bool initProperties() override;
-    virtual void ISGetProperties(const char *dev) override;
+   
     virtual bool ISNewSwitch(const char *dev, const char *name, ISState *states, char *names[], int n) override;
-    virtual bool ISNewText(const char *dev, const char *name, char *texts[], char *names[], int n) override;
     virtual bool updateProperties() override;
-    virtual bool ISSnoopDevice(XMLEle *root) override;
+
 
 protected:
 
@@ -55,7 +54,6 @@ protected:
  
 
     const char *getDefaultName() override;
-    virtual bool saveConfigItems(FILE *fp) override;
     virtual void TimerHit() override;
 
 
