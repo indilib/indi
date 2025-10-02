91e3e35250126887a856e90b6a0a30697fb01545
Jasem Mutlaq <mutlaqja@ikarustech.com>
2025-10-02 16:55:34 +0300

    v2.1.6 release

--- config.h
+++
@@ -1,8 +0,0 @@
-/* Set INDI Library version */
-#define CMAKE_INDI_VERSION_STRING "2.1.5"
-
-/* Define INDI Data Dir */
-#define DATA_INSTALL_DIR "/usr/share/indi/"
-
-/* Set when theora is detected */
-/* #undef HAVE_THEORA */
--- indiversion.h
+++
@@ -1,5 +0,0 @@
-/* Set INDI Library version */
-#define INDI_VERSION 2.1.5
-
-/* Define INDI Data Dir */
-#define DATA_INSTALL_DIR "/usr/share/indi/"
