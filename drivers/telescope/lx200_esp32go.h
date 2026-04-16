73f4b0573fccfc0b1b14ee9e4d773eabf98d7bd8
jordisese <37119245+jordisese@users.noreply.github.com>
2026-04-16 19:57:50 +0200

    lx200_esp32go: improve PINS/TnS compatibility (#2367)
    * lx200_esp32go: improve PINS/TnS compatibility
    
    activate TELESCOPE_CAN_CONTROL_TRACK since it is needed, with a dummy behaviour until FW actually supports it.
    
    * fix warnings

--- drivers/telescope/lx200_esp32go.h
+++ drivers/telescope/lx200_esp32go.h
@@ -44,30 +44,35 @@ class LX200_esp32go : public LX200Generic
         LX200_esp32go();
         ~LX200_esp32go() {}
 
-        const char *getDefaultName();
-        bool initProperties();
-        bool updateProperties();
-        void ISGetProperties(const char *dev);
+        const char *getDefaultName() override;
+        bool initProperties() override;
+        bool updateProperties() override;
+        void ISGetProperties(const char *dev) override;
         //virtual bool ISNewSwitch(const char *dev, const char *name, ISState *states, char *names[], int n); //override;
-        virtual bool ISNewNumber(const char *dev, const char *name, double values[], char *names[], int n);
-        virtual bool updateTime(ln_date *utc, double utc_offset);
+        virtual bool ISNewNumber(const char *dev, const char *name, double values[], char *names[], int n) override;
 
     protected:
-        virtual bool UnPark();
+        virtual bool UnPark() override;
 
         
-        virtual bool ReadScopeStatus(); //override;
-        virtual void getBasicData(); // override;
-        virtual bool SetTrackEnabled(bool enabled); // override;
+        virtual bool ReadScopeStatus() override;
+        virtual void getBasicData() override;
+        virtual bool SetTrackEnabled(bool enabled) override;
+        virtual bool SetTrackMode(uint8_t mode) override;
 
         INDI::PropertyText VersionTP {5};
         INDI::PropertyNumber GuideRateNP {2};
-        //int IsTracking = 0;
         bool guideUpdate = false;
+        bool trackmodeUpdate = false;
         bool picgotoMode = false;
         bool activeFocus = true;
-
-        //virtual bool saveConfigItems(FILE *fp);    
+        enum TelescopeTrackMode
+        {
+            TRACK_SIDEREAL,
+            TRACK_SOLAR,
+            TRACK_LUNAR,
+            TRACK_KING
+        };
 
         long int OSTimeoutSeconds = 0;
         long int OSTimeoutMicroSeconds = 100000;
@@ -80,10 +85,9 @@ class LX200_esp32go : public LX200Generic
         void splitString(const char *str, char array[][100], int *count);
 
         // ------------ FocuserInterface
-        //INDI::PropertyNumber FocusTemperatureNP {2};
-        IPState MoveAbsFocuser (uint32_t targetTicks);// override;
-        IPState MoveRelFocuser (FocusDirection dir, uint32_t ticks);// override;
-        bool AbortFocuser ();// override;
+        IPState MoveAbsFocuser (uint32_t targetTicks) override;
+        IPState MoveRelFocuser (FocusDirection dir, uint32_t ticks) override;
+        bool AbortFocuser () override;
         int UpdateFocuser(); //Return = 0 good, -1 = Communication error
 
 };
